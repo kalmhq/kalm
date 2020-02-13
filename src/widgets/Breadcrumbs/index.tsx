bf6423a760ece20682a84a2a23633cedd66f1782
david <david@ddex.io>
2020-02-13 15:52:26 +0800

    update breadcrumb

--- src/widgets/Breadcrumbs/index.tsx
+++ src/widgets/Breadcrumbs/index.tsx
@@ -1,10 +1,11 @@
-import React from "react";
 import Breadcrumbs from "@material-ui/core/Breadcrumbs";
-import { Link as RouterLink, Route } from "react-router-dom";
 import Link, { LinkProps } from "@material-ui/core/Link";
 import Typography from "@material-ui/core/Typography";
-import styles from "./style.module.css";
 import HomeIcon from "@material-ui/icons/Home";
+import React from "react";
+import { connect } from "react-redux";
+import { Link as RouterLink, Route } from "react-router-dom";
+import styles from "./style.module.css";
 
 const breadcrumbNameMap: { [key: string]: string } = {
   "/applications": "Applications",
@@ -26,27 +27,34 @@ const LinkRouter = (props: LinkRouterProps) => (
   <Link {...props} component={RouterLink as any} />
 );
 
-export const Breadcrumb = () => {
+const BreadcrumbRaw = () => {
   return (
     <Route>
-      {({ location }) => {
-        const pathnames = location.pathname.split("/").filter(x => x);
+      {({ match }) => {
+        const matchedPathNames = match!.path.split("/").filter(x => x);
+
         return (
           <Breadcrumbs aria-label="breadcrumb" className={styles.breadcrumbs}>
             <LinkRouter color="inherit" to="/">
               <HomeIcon fontSize="small" />
             </LinkRouter>
-            {pathnames.map((value, index) => {
-              const last = index === pathnames.length - 1;
-              const to = `/${pathnames.slice(0, index + 1).join("/")}`;
+            {matchedPathNames.map((value, index) => {
+              const last = index === matchedPathNames.length - 1;
+              let to = `/${matchedPathNames.slice(0, index + 1).join("/")}`;
+              let content = breadcrumbNameMap[to];
+
+              if (value.startsWith(":")) {
+                content = match!.params[value.slice(1)];
+                to = to.replace(value, match!.params[value.slice(1)]);
+              }
 
               return last ? (
                 <Typography color="textPrimary" key={to}>
-                  {breadcrumbNameMap[to]}
+                  {content}
                 </Typography>
               ) : (
                 <LinkRouter color="inherit" to={to} key={to}>
-                  {breadcrumbNameMap[to]}
+                  {content}
                 </LinkRouter>
               );
             })}
@@ -56,3 +64,5 @@ export const Breadcrumb = () => {
     </Route>
   );
 };
+
+export const Breadcrumb = connect()(BreadcrumbRaw);
