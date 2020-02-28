53db8be7019959b032f17036f80df9ff8a8796b4
Liu Mingmin <liumm@ddex.io>
2020-02-28 11:45:02 +0800

    add CRD: Dependency
    kubebuilder create api --group core --version v1alpha1 --kind Dependency

--- main.go
+++ main.go
@@ -80,6 +80,14 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "File")
 		os.Exit(1)
 	}
+	if err = (&controllers.DependencyReconciler{
+		Client: mgr.GetClient(),
+		Log:    ctrl.Log.WithName("controllers").WithName("Dependency"),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "Dependency")
+		os.Exit(1)
+	}
 	// +kubebuilder:scaffold:builder
 
 	setupLog.Info("starting manager")
