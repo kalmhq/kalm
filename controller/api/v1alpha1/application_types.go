28d9b67d56817d6efd102c3762965d4395e2a169
wanglei <wanglei@ddex.io>
2020-03-11 23:27:19 +0800

    ok fix ci

--- controller/api/v1alpha1/application_types.go
+++ controller/api/v1alpha1/application_types.go
@@ -23,7 +23,7 @@ import (
 	"k8s.io/apimachinery/pkg/runtime"
 )
 
-// EDIT THIS FILE! THIS IS SCAFFOLDING FOR YOU TO OWN!
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
 // NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
 
 type ComponentSpec struct {
