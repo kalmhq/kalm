f33eba2fc5cf461dd7a61dc2826c72707c2d4a0d
david <davidqhr@gmail.com>
2020-03-19 00:52:34 +0800

    merge

--- api/handler/k8s.go
+++ api/handler/k8s.go
@@ -5,6 +5,28 @@ import (
 	"k8s.io/metrics/pkg/client/clientset/versioned"
 )
 
+// func (h *ApiHandler) handleGetRoles(c echo.Context) error {
+// 	k8sClient := getK8sClient(c)
+// 	res, err := k8sClient.RESTClient().Get().AbsPath("/apis/rbac.authorization.k8s.io/v1/namespaces/default/roles").DoRaw()
+
+// 	if err != nil {
+// 		return err
+// 	}
+
+// 	return c.JSONBlob(200, res)
+// }
+
+// func (h *ApiHandler) handleGetRoleBindings(c echo.Context) error {
+// 	k8sClient := getK8sClient(c)
+// 	res, err := k8sClient.RESTClient().Get().AbsPath("/apis/rbac.authorization.k8s.io/v1/namespaces/default/clusterrolebindings").DoRaw()
+
+// 	if err != nil {
+// 		return err
+// 	}
+
+// 	return c.JSONBlob(200, res)
+// }
+
 func (h *ApiHandler) handleGetDeployments(c echo.Context) error {
 	k8sClient := getK8sClient(c)
 	list, err := k8sClient.AppsV1().Deployments("").List(ListAll)
@@ -46,4 +68,92 @@ func (h *ApiHandler) handleGetNodeMetrics(c echo.Context) error {
 	}
 
 	return c.JSON(200, nodeMetrics)
+}
+
+func (h *ApiHandler) handleGetClusterRoles(c echo.Context) error {
+	k8sClient := getK8sClient(c)
+	res, err := k8sClient.RESTClient().Get().AbsPath("/apis/rbac.authorization.k8s.io/v1/clusterroles").DoRaw()
+
+	if err != nil {
+		return err
+	}
+
+	return c.JSONBlob(200, res)
+}
+
+func (h *ApiHandler) handleCreateClusterRoles(c echo.Context) error {
+	k8sClient := getK8sClient(c)
+	res, err := k8sClient.RESTClient().Post().Body(c.Request().Body).AbsPath("/apis/rbac.authorization.k8s.io/v1/clusterroles").DoRaw()
+
+	if err != nil {
+		return err
+	}
+
+	return c.JSONBlob(200, res)
+}
+
+func (h *ApiHandler) handleGetClusterRoleBindings(c echo.Context) error {
+	k8sClient := getK8sClient(c)
+	res, err := k8sClient.RESTClient().Get().AbsPath("/apis/rbac.authorization.k8s.io/v1/clusterrolebindings").DoRaw()
+
+	if err != nil {
+		return err
+	}
+
+	return c.JSONBlob(200, res)
+}
+
+func (h *ApiHandler) handleCreateClusterRoleBindings(c echo.Context) error {
+	k8sClient := getK8sClient(c)
+	res, err := k8sClient.RESTClient().Post().Body(c.Request().Body).AbsPath("/apis/rbac.authorization.k8s.io/v1/clusterrolebindings").DoRaw()
+
+	if err != nil {
+		return err
+	}
+
+	return c.JSONBlob(200, res)
+}
+
+func (h *ApiHandler) handleGetServiceAccounts(c echo.Context) error {
+	k8sClient := getK8sClient(c)
+	res, err := k8sClient.RESTClient().Get().AbsPath("/api/v1/namespaces/default/serviceaccounts").DoRaw()
+
+	if err != nil {
+		return err
+	}
+
+	return c.JSONBlob(200, res)
+}
+
+func (h *ApiHandler) handleCreateServiceAccounts(c echo.Context) error {
+	k8sClient := getK8sClient(c)
+	res, err := k8sClient.RESTClient().Post().Body(c.Request().Body).AbsPath("/api/v1/namespaces/default/serviceaccounts").DoRaw()
+
+	if err != nil {
+		return err
+	}
+
+	return c.JSONBlob(200, res)
+}
+
+func (h *ApiHandler) handleGetSecrets(c echo.Context) error {
+	k8sClient := getK8sClient(c)
+	res, err := k8sClient.RESTClient().Get().AbsPath("/api/v1/namespaces/default/secrets").DoRaw()
+
+	if err != nil {
+		return err
+	}
+
+	return c.JSONBlob(200, res)
+}
+
+func (h *ApiHandler) handleCreateSecrets(c echo.Context) error {
+	k8sClient := getK8sClient(c)
+	res, err := k8sClient.RESTClient().Post().Body(c.Request().Body).AbsPath("/api/v1/namespaces/default/secrets").DoRaw()
+
+	if err != nil {
+		return err
+	}
+
+	return c.JSONBlob(200, res)
 }
