1a0fbdf2573c9651d85794325a9b04337b8af2b6
david <davidqhr@gmail.com>
2020-04-09 00:34:47 +0800

    remove duplicated apis

--- api/handler/handler.go
+++ api/handler/handler.go
@@ -37,12 +37,6 @@ func (h *ApiHandler) Install(e *echo.Echo) {
 	gV1.GET("/nodes/metrics", h.handleGetNodeMetrics)
 	gV1.GET("/persistentvolumes", h.handleGetPVs)
 
-	// kapp resources
-	gV1.GET("/componenttemplates", h.handleGetComponentTemplates)
-	gV1.POST("/componenttemplates", h.handleCreateComponentTemplate)
-	gV1.PUT("/componenttemplates/:name", h.handleUpdateComponentTemplate)
-	gV1.DELETE("/componenttemplates/:name", h.handleDeleteComponentTemplate)
-
 	gV1.GET("/dependencies", h.handleGetDependencies)
 	gV1.GET("/dependencies/available", h.handleGetAvailableDependencies)
 
@@ -83,10 +77,10 @@ func (h *ApiHandler) Install(e *echo.Echo) {
 	gv1Alpha1WithAuth.DELETE("/applications/:namespace/:name", h.handleDeleteApplication)
 	gv1Alpha1WithAuth.POST("/applications/:namespace", h.handleCreateApplicationNew)
 
-	gv1Alpha1WithAuth.GET("/componenttemplates", h.handleGetComponentTemplatesNew)
-	gv1Alpha1WithAuth.POST("/componenttemplates", h.handleCreateComponentTemplateNew)
-	gv1Alpha1WithAuth.PUT("/componenttemplates/:name", h.handleUpdateComponentTemplateNew)
-	gv1Alpha1WithAuth.DELETE("/componenttemplates/:name", h.handleDeleteComponentTemplateNew)
+	gv1Alpha1WithAuth.GET("/componenttemplates", h.handleGetComponentTemplates)
+	gv1Alpha1WithAuth.POST("/componenttemplates", h.handleCreateComponentTemplate)
+	gv1Alpha1WithAuth.PUT("/componenttemplates/:name", h.handleUpdateComponentTemplate)
+	gv1Alpha1WithAuth.DELETE("/componenttemplates/:name", h.handleDeleteComponentTemplate)
 
 	gv1Alpha1WithAuth.GET("/files/:namespace", h.handleListFiles)
 	gv1Alpha1WithAuth.POST("/files/:namespace", h.handleCreateFile)
