d57c5ec351c701abe8196c23c7c5e9d031647d2d
Aladdin <ialaddin@me.com>
2020-04-11 03:04:53 +0800

    unified add button style

--- frontend/src/widgets/AddButton.tsx
+++ frontend/src/widgets/AddButton.tsx
@@ -1,22 +1,14 @@
 import { Link } from "react-router-dom";
-import { Button, ButtonProps } from "@material-ui/core";
-import AddIcon from "@material-ui/icons/Add";
+import { IconButton } from "@material-ui/core";
+import AddCircleIcon from "@material-ui/icons/AddCircle";
 import React from "react";
 
 export const AddLink = (props: { to: string }) => {
   return (
     <Link to={props.to}>
-      <Button color="primary" size="large" startIcon={<AddIcon />} onClick={() => {}}>
-        Add
-      </Button>
+      <IconButton>
+        <AddCircleIcon color="primary" />
+      </IconButton>
     </Link>
-  );
-};
-
-export const AddButton = (props: React.PropsWithChildren<React.RefAttributes<SVGSVGElement>> & ButtonProps) => {
-  return (
-    <Button ref={props.ref} color="primary" size="large" startIcon={<AddIcon />} onClick={props.onClick}>
-      Add
-    </Button>
   );
 };
