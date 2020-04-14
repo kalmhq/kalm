657cdbeb2fdda90f1b8cb2bdb473247244bef4f7
Liu Mingmin <liumm@ddex.io>
2020-04-14 14:14:14 +0800

    test commit on github

--- controller/api/v1alpha1/validate_kapp.go
+++ controller/api/v1alpha1/validate_kapp.go
@@ -1,5 +1,14 @@
 package v1alpha1
 
+type KappValidateError struct {
+	err string
+	path string
+}
+
+func (k KappValidateError) Error() string {
+	return k.err
+}
+
 func TryValidateApplication(appSpec ApplicationSpec) error {
 	// for now only check dependency here
 	validateFuncs := []func(spec ApplicationSpec) error{isValidateDependency}
