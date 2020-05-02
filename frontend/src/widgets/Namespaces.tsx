dc65c40a12aef9a557fd7e491a3ea3169dd9563b
Aladdin <ialaddin@me.com>
2020-05-02 17:15:11 +0800

    Fixed select a namespace title

--- frontend/src/widgets/Namespaces.tsx
+++ frontend/src/widgets/Namespaces.tsx
@@ -15,6 +15,7 @@ import React from "react";
 import { connect } from "react-redux";
 import { TDispatchProp } from "types";
 import { setCurrentNamespaceAction } from "actions/namespaces";
+import { push } from "connected-react-router";
 import { RootState } from "reducers";
 import ExpandLess from "@material-ui/icons/ExpandLess";
 import ExpandMore from "@material-ui/icons/ExpandMore";
@@ -90,59 +91,88 @@ class NamespacesRaw extends React.PureComponent<Props, State> {
   public render() {
     const { classes, applications, active, dispatch, isListFirstLoading } = this.props;
     const { open } = this.state;
+    const hasApplication = applications.count() > 0;
     const activeNamespace = applications.find(application => application.get("name") === active);
-    return (
-      <div className={classes.root}>
-        <Button
-          ref={this.anchorRef}
-          aria-controls={open ? "menu-list-grow" : undefined}
-          aria-haspopup="true"
-          className={classes.namespaceButton}
-          onClick={this.handleToggle}>
-          <H4>
-            {isListFirstLoading ? "Loading..." : activeNamespace ? activeNamespace.get("name") : "Select a namespace"}
-          </H4>
-          {open ? <ExpandLess /> : <ExpandMore />}
-        </Button>
 
-        <Popper
-          open={open}
-          anchorEl={this.anchorRef.current}
-          role={undefined}
-          placement="bottom-start"
-          transition
-          disablePortal>
-          {({ TransitionProps, placement }) => (
-            <Grow
-              {...TransitionProps}
-              style={{ transformOrigin: placement === "bottom" ? "center top" : "center bottom" }}>
-              <Paper>
-                <ClickAwayListener onClickAway={this.handleClose}>
-                  <MenuList
-                    autoFocusItem={open}
-                    id="menu-list-grow"
-                    onKeyDown={this.handleListKeyDown}
-                    classes={{ root: classes.menuList }}>
-                    {applications
-                      .map(application => (
-                        <MenuItem
-                          onClick={() => {
-                            dispatch(setCurrentNamespaceAction(application.get("name")));
-                            this.handleClose();
-                          }}
-                          key={application.get("name")}>
-                          <H4>{application.get("name")}</H4>
-                        </MenuItem>
-                      ))
-                      .toArray()}
-                  </MenuList>
-                </ClickAwayListener>
-              </Paper>
-            </Grow>
-          )}
-        </Popper>
-      </div>
-    );
+    if (isListFirstLoading) {
+      return (
+        <div className={classes.root}>
+          <Button className={classes.namespaceButton}>
+            <H4>Loading...</H4>
+          </Button>
+        </div>
+      );
+    }
+    if (!isListFirstLoading && hasApplication) {
+      return (
+        <div className={classes.root}>
+          <Button
+            ref={this.anchorRef}
+            aria-controls={open ? "menu-list-grow" : undefined}
+            aria-haspopup="true"
+            className={classes.namespaceButton}
+            onClick={this.handleToggle}>
+            <H4>
+              {isListFirstLoading
+                ? "Loading..."
+                : activeNamespace
+                ? activeNamespace.get("name")
+                : "Select a Application"}
+            </H4>
+            {open ? <ExpandLess /> : <ExpandMore />}
+          </Button>
+
+          <Popper
+            open={open}
+            anchorEl={this.anchorRef.current}
+            role={undefined}
+            placement="bottom-start"
+            transition
+            disablePortal>
+            {({ TransitionProps, placement }) => (
+              <Grow
+                {...TransitionProps}
+                style={{ transformOrigin: placement === "bottom" ? "center top" : "center bottom" }}>
+                <Paper>
+                  <ClickAwayListener onClickAway={this.handleClose}>
+                    <MenuList
+                      autoFocusItem={open}
+                      id="menu-list-grow"
+                      onKeyDown={this.handleListKeyDown}
+                      classes={{ root: classes.menuList }}>
+                      {applications
+                        .map(application => (
+                          <MenuItem
+                            onClick={() => {
+                              dispatch(setCurrentNamespaceAction(application.get("name")));
+                              this.handleClose();
+                            }}
+                            key={application.get("name")}>
+                            <H4>{application.get("name")}</H4>
+                          </MenuItem>
+                        ))
+                        .toArray()}
+                    </MenuList>
+                  </ClickAwayListener>
+                </Paper>
+              </Grow>
+            )}
+          </Popper>
+        </div>
+      );
+    } else {
+      return (
+        <div className={classes.root}>
+          <Button
+            className={classes.namespaceButton}
+            onClick={() => {
+              dispatch(push(`/applications/new`));
+            }}>
+            <H4>Create Your First Application</H4>
+          </Button>
+        </div>
+      );
+    }
   }
 }
 
