4bbe5c6b51e0909ed731c8a0b7a46f9116428b3c
david <davidqhr@gmail.com>
2020-05-27 17:05:31 +0800

    refactor http route controller

--- controller/controllers/http_route_controller_test.go
+++ controller/controllers/http_route_controller_test.go
@@ -1,10 +1,8 @@
 package controllers
 
 import (
-	"context"
 	"github.com/kapp-staging/kapp/controller/api/v1alpha1"
 	"github.com/stretchr/testify/suite"
-	"istio.io/client-go/pkg/apis/networking/v1beta1"
 	coreV1 "k8s.io/api/core/v1"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"testing"
@@ -28,10 +26,14 @@ func (suite *HttpRouteControllerSuite) SetupSuite() {
 	suite.ns = &ns
 }
 
+func TestRegexp(t *testing.T) {
+}
+
 func (suite *HttpRouteControllerSuite) TestCertDomainMatchHost() {
 	host := "www.example.com"
 
 	suite.True(certCanBeUsedOnDomain([]string{
+
 		"www.example.com",
 	}, host))
 
@@ -73,9 +75,7 @@ func (suite *HttpRouteControllerSuite) TestBasicHttpRoute() {
 					Value:    "application/json",
 				},
 			},
-			StripPath:           true,
-			HttpRedirectToHttps: true,
-			Certification:       "Auto",
+			StripPath: true,
 			Destinations: []v1alpha1.HttpRouteDestination{
 				{
 					Host:   "test:80",
@@ -86,48 +86,6 @@ func (suite *HttpRouteControllerSuite) TestBasicHttpRoute() {
 	}
 
 	suite.createObject(&route)
-
-	var gateways v1beta1.GatewayList
-	suite.Eventually(func() bool {
-		suite.Nil(suite.K8sClient.List(context.Background(), &gateways))
-		return len(gateways.Items) == 1
-	})
-
-	gateway := gateways.Items[0]
-	suite.Equal(getHttpRouteHttpGatewayName(route.Name), gateway.Name)
-
-	var virtualServices v1beta1.VirtualServiceList
-	suite.Eventually(func() bool {
-		suite.Nil(suite.K8sClient.List(context.Background(), &virtualServices))
-		return len(virtualServices.Items) == 1
-	})
-
-	virtualService := virtualServices.Items[0]
-	suite.Equal(route.Name, virtualService.Name)
-
-	// rewrite
-	suite.NotNil(virtualService.Spec.Http[0].Rewrite)
-	suite.Equal(virtualService.Spec.Http[0].Rewrite.Uri, "/")
-
-	// match
-	suite.Len(virtualService.Spec.Http[0].Match, len(route.Spec.Paths)*len(route.Spec.Methods))
-
-	// route
-	suite.Len(virtualService.Spec.Http[0].Route, 1)
-	suite.Nil(virtualService.Spec.Http[0].Retries)
-	suite.Nil(virtualService.Spec.Http[0].Mirror)
-	suite.Nil(virtualService.Spec.Http[0].Fault)
-	suite.Nil(virtualService.Spec.Http[0].CorsPolicy)
-	suite.Nil(virtualService.Spec.Http[0].Timeout)
-
-	//// create cert issuer
-	//issuer := genEmptyCAHttpsCertIssuer()
-	//suite.createHttpsCertIssuer(issuer)
-	//
-	//// create cert
-	//httpsCert := genHttpsCert(issuer.Name)
-	//httpsCert.Spec.Domains = []string{"example.com"}
-	//suite.createHttpsCert(httpsCert)
 }
 
 func TestHttpRouteControllerSuite(t *testing.T) {
