743b7815a519931b4e11c25631251eae8989743a
Liu Mingmin <liumm@ddex.io>
2020-05-28 21:35:25 +0800

    reconcile default issuer & httpscert

--- controller/controllers/kapp_ns_controller.go
+++ controller/controllers/kapp_ns_controller.go
@@ -17,10 +17,14 @@ package controllers
 
 import (
 	"context"
-	"fmt"
 	"github.com/kapp-staging/kapp/controller/api/v1alpha1"
 	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
+	"sigs.k8s.io/controller-runtime/pkg/source"
 	"strconv"
 	"time"
 
@@ -35,21 +39,31 @@ const (
 // KappNSReconciler watches all namespaces
 type KappNSReconciler struct {
 	*BaseReconciler
+	ctx context.Context
 }
 
 func NewKappNSReconciler(mgr ctrl.Manager) *KappNSReconciler {
 	return &KappNSReconciler{
-		NewBaseReconciler(mgr, "KappNS"),
+		BaseReconciler: NewBaseReconciler(mgr, "KappNS"),
+		ctx:            context.Background(),
 	}
+}
+
+func (r *KappNSReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewControllerManagedBy(mgr).
+		For(&v1.Namespace{}).
+		Watches(&source.Kind{Type: &v1alpha1.HttpsCertIssuer{}}, &handler.EnqueueRequestForObject{}).
+		Watches(&source.Kind{Type: &v1alpha1.HttpsCert{}}, &handler.EnqueueRequestForObject{}).
+		//Owns(&v1alpha1.HttpsCert{}).
+		Complete(r)
 }
 
 // +kubebuilder:rbac:groups="",resources=namespaces,verbs=get;list;watch;create;update;patch;delete
 func (r *KappNSReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
-	ctx := context.Background()
-	_ = r.Log.WithValues("kappns", req.NamespacedName)
+	ctx := r.ctx
+	logger := r.Log.WithValues("kappns", req.NamespacedName)
 
-	// your logic here
-	fmt.Println("kapp ns reconciling...")
+	logger.Info("kapp ns reconciling...")
 
 	var namespaceList v1.NamespaceList
 	if err := r.List(ctx, &namespaceList); err != nil {
@@ -93,11 +107,56 @@ func (r *KappNSReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
 		}
 	}
 
+	// check if default caIssuer & cert is created
+	if err := r.reconcileDefaultCAIssuerAndCert(); err != nil {
+		return ctrl.Result{}, err
+	}
+
 	return ctrl.Result{}, nil
 }
 
-func (r *KappNSReconciler) SetupWithManager(mgr ctrl.Manager) error {
-	return ctrl.NewControllerManagedBy(mgr).
-		For(&v1.Namespace{}).
-		Complete(r)
+func (r *KappNSReconciler) reconcileDefaultCAIssuerAndCert() error {
+	defaultCAIssuerName := "default-cert-issuer"
+
+	caIssuer := v1alpha1.HttpsCertIssuer{}
+	err := r.Get(r.ctx, types.NamespacedName{Name: defaultCAIssuerName}, &caIssuer)
+	if errors.IsNotFound(err) {
+		caIssuer = v1alpha1.HttpsCertIssuer{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: defaultCAIssuerName,
+			},
+			Spec: v1alpha1.HttpsCertIssuerSpec{
+				CAForTest: &v1alpha1.CAForTestIssuer{},
+			},
+		}
+
+		if err := r.Create(r.ctx, &caIssuer); err != nil {
+			return err
+		}
+	}
+
+	defaultCertName := "default-https-cert"
+	expectedCert := v1alpha1.HttpsCert{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: defaultCertName,
+		},
+		Spec: v1alpha1.HttpsCertSpec{
+			HttpsCertIssuer: defaultCAIssuerName,
+			Domains:         []string{"*"},
+		},
+	}
+
+	var currentCert v1alpha1.HttpsCert
+	if err = r.Get(r.ctx, types.NamespacedName{Name: defaultCertName}, &currentCert); err != nil {
+		if !errors.IsNotFound(err) {
+			return err
+		}
+
+		return r.Create(r.ctx, &expectedCert)
+	} else {
+		currentCert.Spec = expectedCert.Spec
+		return r.Update(r.ctx, &currentCert)
+	}
+
+	//return r.Patch(r.ctx, &currentCert, client.MergeFrom(&expectedCert))
 }
