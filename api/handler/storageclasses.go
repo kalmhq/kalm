c5ede6ec8f3be09bf6b2c7d8044340ee56bb3de6
Liu Mingmin <liumm@ddex.io>
2020-06-08 21:50:38 +0800

    WIP api for volumes

---
+++ api/handler/storageclasses.go
@@ -0,0 +1,8 @@
+package handler
+
+import "github.com/labstack/echo/v4"
+
+func (h *ApiHandler) handleListStorageClasses(c echo.Context) error {
+	//todo
+	return c.JSON(200, []int{})
+}
---
+++ api/handler/volumes.go
@@ -0,0 +1,13 @@
+package handler
+
+import "github.com/labstack/echo/v4"
+
+func (h *ApiHandler) handleListVolumes(context echo.Context) error {
+	//todo
+	return nil
+}
+
+func (h *ApiHandler) handleReleasedPV(context echo.Context) error {
+	//todo
+	return nil
+}
