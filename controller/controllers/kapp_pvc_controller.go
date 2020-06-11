30eb4a6eecdade553a5cba9a29ec1b351630fe34
Liu Mingmin <liumm@ddex.io>
2020-06-11 21:58:36 +0800

    add tests to make sure pvc pv works as expected

--- controller/controllers/kapp_pvc_controller.go
+++ controller/controllers/kapp_pvc_controller.go
@@ -21,13 +21,11 @@ import (
 	corev1 "k8s.io/api/core/v1"
 	v1 "k8s.io/api/storage/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
+	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/handler"
 	"sigs.k8s.io/controller-runtime/pkg/source"
 	"strings"
-	"time"
-
-	ctrl "sigs.k8s.io/controller-runtime"
 )
 
 const (
@@ -55,31 +53,22 @@ func NewKappPVCReconciler(mgr ctrl.Manager) *KappPVCReconciler {
 // +kubebuilder:rbac:groups=storage.k8s.io,resources=storageclasses,verbs=get;list;watch;create;update;patch;delete
 
 func (r *KappPVCReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
-	r.Log.Info("reconciling pvc volumes")
+	r.Log.Info("reconciling kappPvc volumes", "req", req)
 
 	ctx := context.Background()
 	log := r.Log.WithValues("kapppvc", req.NamespacedName)
 
-	// 0. prepare storage class
-	cloudProvider, ok := r.guessCurrentCloudProvider()
-	if !ok {
-		log.Info("fail to find current cloudProvier")
-		return ctrl.Result{}, nil
-	}
-
-	err := r.reconcileDefaultStorageClass(cloudProvider)
-	if err != nil {
+	// 1. check if any Component is using this kappPvc, if not delete it
+	// todo skip delete if reclaim policy of kappPV is not Retain
+	//      or if storage class of PV is not Kapp-Managed
+	var kappPvcList corev1.PersistentVolumeClaimList
+	err := r.List(ctx, &kappPvcList, client.MatchingLabels{KappLabelManaged: "true"})
+	if client.IgnoreNotFound(err) != nil {
+		//fmt.Println("unexpected err:", err)
 		return ctrl.Result{}, err
 	}
 
-	// 1. check if any Component is using this pvc, if not delete it
-	// todo skip delete if reclaim policy of pv is not Retain
-	//      or if storage class of PV is not Kapp-Managed
-	var pvcList corev1.PersistentVolumeClaimList
-	err = r.List(ctx, &pvcList, client.MatchingLabels{"kapp-managed": "true"})
-	if err != nil {
-		return ctrl.Result{}, client.IgnoreNotFound(err)
-	}
+	//fmt.Println("kappPVC:", len(kappPvcList.Items), kappPvcList.Items)
 
 	var componentList v1alpha1.ComponentList
 	if err = r.List(ctx, &componentList); err != nil {
@@ -89,86 +78,100 @@ func (r *KappPVCReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
 	}
 
 	var activePVCs []corev1.PersistentVolumeClaim
-	var ownerComponents []v1alpha1.Component
 
-	for _, pvc := range pvcList.Items {
-		if comp, exist := findComponentUsingPVC(pvc, componentList); exist {
-			activePVCs = append(activePVCs, pvc)
-			ownerComponents = append(ownerComponents, comp)
+	for _, kappPvc := range kappPvcList.Items {
+		if _, exist := findComponentUsingPVC(kappPvc, componentList); exist {
+			activePVCs = append(activePVCs, kappPvc)
 
 			continue
 		}
 
-		// todo more careful deleting this pvc
-		r.Log.Info("deleting un-used pvc", "pvc", pvc.Name, "comps", componentList.Items)
-		if err := r.Delete(ctx, &pvc); err != nil {
+		// todo more careful deleting this kappPvc
+		r.Log.Info("deleting un-used kappPvc", "kappPvc", kappPvc.Name, "comps", componentList.Items)
+		if err := r.Delete(ctx, &kappPvc); err != nil {
 			return ctrl.Result{}, nil
 		}
 	}
 
 	// 2. PV
 
-	var pvList corev1.PersistentVolumeList
-	if err := r.List(ctx, &pvList); err != nil {
+	var kappPVList corev1.PersistentVolumeList
+	if err := r.List(ctx, &kappPVList, client.MatchingLabels{KappLabelManaged: "true"}); err != nil {
 		return ctrl.Result{}, client.IgnoreNotFound(err)
 	}
 
 	// 2.1 for all kapp PV in Released stats, clean claimRef to make it available again
-	for _, pv := range pvList.Items {
-		if _, exist := pv.Labels[KappLabelManaged]; !exist {
+	for _, kappPV := range kappPVList.Items {
+		if kappPV.Status.Phase != corev1.VolumeReleased {
 			continue
 		}
 
-		if pv.Status.Phase != corev1.VolumeReleased {
-			continue
-		}
+		if kappPV.Spec.ClaimRef != nil {
+			kappPV.Spec.ClaimRef = nil
 
-		if pv.Spec.ClaimRef != nil {
-			pv.Spec.ClaimRef = nil
-
-			if err := r.Update(ctx, &pv); err != nil {
+			if err := r.Update(ctx, &kappPV); err != nil {
 				return ctrl.Result{}, nil
 			}
 		}
 	}
 
-	// 2.2 make sure for each active pvc, underlying pv is labeled with its name
+	// 2.2 make sure for each active kappPvc, underlying kappPV is labeled with its name
 	//     (to be selected using selector)
-	for i, activePVC := range activePVCs {
+	var pvList corev1.PersistentVolumeList
+	if err := r.List(ctx, &pvList); client.IgnoreNotFound(err) != nil {
+		return ctrl.Result{}, err
+	}
 
-		hasFoundPV := false
+	//fmt.Println("aaaa", len(pvList.Items), pvList.Items, req)
 
-		for _, pv := range pvList.Items {
-			if pv.Name != activePVC.Spec.VolumeName {
-				continue
-			}
-
-			hasFoundPV = true
-
-			if pv.Labels == nil {
-				pv.Labels = make(map[string]string)
-			}
-
-			ownerComp := ownerComponents[i]
-
-			pv.Labels[KappLabelManaged] = "true"
-			pv.Labels[KappLabelComponent] = ownerComp.Name
-			pv.Labels[KappLabelNamespace] = ownerComp.Namespace
-			// to be selectable by PVC
-			pv.Labels[KappLabelPV] = pv.Name
-
-			if err := r.Update(ctx, &pv); err != nil {
-				return ctrl.Result{}, err
-			}
+	for _, pv := range pvList.Items {
+		kappPVC, isRefed := isReferencedByKappPVC(pv, kappPvcList)
+		if !isRefed {
+			log.Info("pv is not refed, skipped", "pv:", pv.Name)
+			continue
 		}
 
-		if !hasFoundPV {
-			r.Log.Info("fail to find PV for PVC, requeue to try later", "pvc", activePVC.Name)
-			return ctrl.Result{RequeueAfter: 1 * time.Second}, nil
+		if pv.Labels == nil {
+			pv.Labels = make(map[string]string)
+		}
+
+		pv.Labels[KappLabelManaged] = "true"
+		pv.Labels[KappLabelComponent] = kappPVC.Labels[KappLabelComponent]
+		pv.Labels[KappLabelNamespace] = kappPVC.Labels[KappLabelNamespace]
+		// to be selectable by PVC
+		pv.Labels[KappLabelPV] = pv.Name
+
+		if err := r.Update(ctx, &pv); err != nil {
+			return ctrl.Result{}, err
 		}
 	}
 
+	// 3. prepare storage class
+	cloudProvider, ok := r.guessCurrentCloudProvider()
+	if !ok {
+		log.Info("fail to find current cloudProvier")
+		return ctrl.Result{}, nil
+	}
+
+	err = r.reconcileDefaultStorageClass(cloudProvider)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
 	return ctrl.Result{}, nil
+}
+
+func isReferencedByKappPVC(pv corev1.PersistentVolume, list corev1.PersistentVolumeClaimList,
+) (pvc corev1.PersistentVolumeClaim, isRefed bool) {
+
+	for _, pvc := range list.Items {
+		if pvc.Spec.VolumeName == pv.Name {
+			return pvc, true
+		}
+
+	}
+
+	return
 }
 
 func findComponentUsingPVC(pvc corev1.PersistentVolumeClaim, compList v1alpha1.ComponentList) (v1alpha1.Component, bool) {
@@ -194,9 +197,9 @@ func findComponentUsingPVC(pvc corev1.PersistentVolumeClaim, compList v1alpha1.C
 
 func (r *KappPVCReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
-		For(&corev1.PersistentVolume{}).
-		For(&v1.StorageClass{}).
-		//For(&corev1.PersistentVolumeClaim{}).
+		//For(&corev1.PersistentVolume{}).
+		//For(&v1.StorageClass{}).
+		For(&corev1.PersistentVolumeClaim{}).
 		Watches(
 			&source.Kind{Type: &corev1.PersistentVolumeClaim{}},
 			&handler.EnqueueRequestForObject{},
@@ -204,6 +207,14 @@ func (r *KappPVCReconciler) SetupWithManager(mgr ctrl.Manager) error {
 		//For(&v1alpha1.Component{}).
 		Watches(
 			&source.Kind{Type: &v1alpha1.Component{}},
+			&handler.EnqueueRequestForObject{},
+		).
+		Watches(
+			&source.Kind{Type: &corev1.PersistentVolume{}},
+			&handler.EnqueueRequestForObject{},
+		).
+		Watches(
+			&source.Kind{Type: &v1.StorageClass{}},
 			&handler.EnqueueRequestForObject{},
 		).
 		Complete(r)
