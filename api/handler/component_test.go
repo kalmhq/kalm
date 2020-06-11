9f4d8c6ee29f531f8d5cc7504c8153b75c04f4b4
Liu Mingmin <liumm@ddex.io>
2020-06-11 14:30:14 +0800

    add test for reuse pv

--- api/handler/component_test.go
+++ api/handler/component_test.go
@@ -49,20 +49,20 @@ func (suite *ComponentTestSuite) TeardownSuite() {
 	})
 }
 
-func (suite *ComponentTestSuite) TestGetEmptyComponentList() {
-	rec := suite.NewRequest(http.MethodGet, "/v1alpha1/applications/testKapp/components", nil)
-
-	var res []resources.Component
-	rec.BodyAsJSON(&res)
-
-	suite.Equal(200, rec.Code)
-}
+//func (suite *ComponentTestSuite) TestGetEmptyComponentList() {
+//	rec := suite.NewRequest(http.MethodGet, "/v1alpha1/applications/testKapp/components", nil)
+//
+//	var res []resources.Component
+//	rec.BodyAsJSON(&res)
+//
+//	suite.Equal(200, rec.Code)
+//}
 
 func (suite *ComponentTestSuite) TestCreateComponentWithPVCAsVolume() {
 
 	sc := "kapp-standard"
 	reqComp := resources.Component{
-		Name: "foobar",
+		Name: "foobar-create-new-pv",
 		ComponentSpec: v1alpha1.ComponentSpec{
 			Image: "foo",
 			Volumes: []v1alpha1.Volume{
@@ -104,5 +104,78 @@ func (suite *ComponentTestSuite) TestCreateComponentWithPVCAsVolume() {
 }
 
 func (suite *ComponentTestSuite) TestCreateComponentWithReUsingPVCAsVolume() {
-	//todo
+	scName := "kapp-standard"
+	pvNameToReuse := "existAvailablePV"
+
+	// prepare PV & scName
+	//sc := storagev1.StorageClass{
+	//	ObjectMeta: v1.ObjectMeta{
+	//		Name: scName,
+	//	},
+	//	Provisioner: "fake-provisioner",
+	//}
+	//
+	//_, err := suite.k8sClinet.StorageV1().StorageClasses().Create(&sc)
+	//suite.Nil(err)
+
+	// prepare pv
+	pv := coreV1.PersistentVolume{
+		ObjectMeta: v1.ObjectMeta{
+			Name: pvNameToReuse,
+		},
+		Spec: coreV1.PersistentVolumeSpec{
+			PersistentVolumeSource: coreV1.PersistentVolumeSource{
+			},
+		},
+	}
+	_, err = suite.k8sClinet.CoreV1().PersistentVolumes().Create(&pv)
+	suite.Nil(err)
+
+	reqComp := resources.Component{
+		Name: "foobar-reuse-pv",
+		ComponentSpec: v1alpha1.ComponentSpec{
+			Image: "foo",
+			Volumes: []v1alpha1.Volume{
+				{
+					Path:                           "/data",
+					Size:                           resource.MustParse("1Mi"),
+					Type:                           v1alpha1.VolumeTypePersistentVolumeClaim,
+					StorageClassName:               &scName,
+					PersistentVolumeNamePVCToMatch: "existAvailablePV",
+				},
+			},
+		},
+	}
+
+	compInJSON, err := json.Marshal(reqComp)
+	suite.Nil(err)
+
+	apiURL := fmt.Sprintf("/v1alpha1/applications/%s/components", nsName)
+
+	rec := suite.NewRequest(http.MethodPost, apiURL, string(compInJSON))
+
+	var res resources.Component
+	rec.BodyAsJSON(&res)
+
+	suite.Equal(201, rec.Code)
+
+	// check if volume in Comp is as expected
+	compList, err := suite.getComponentList(nsName)
+	suite.Nil(err)
+	suite.Equal(1, len(compList.Items))
+
+	comp := compList.Items[0]
+	suite.Equal(1, len(comp.Spec.Volumes))
+
+	expectedPVCName := res.Volumes[0].PersistentVolumeClaimName
+	suite.Equal(expectedPVCName, comp.Spec.Volumes[0].PersistentVolumeClaimName)
+
+	suite.Equal(scName, *comp.Spec.Volumes[0].StorageClassName)
+	suite.Equal(reqComp.Volumes[0].PersistentVolumeNamePVCToMatch, comp.Spec.Volumes[0].PersistentVolumeNamePVCToMatch)
+
+}
+
+func (suite *ComponentTestSuite) TestCreateComponentWithReusePVInvalid() {
+	// todo
+	// 1. pv try to reuse is not available or not exist
 }
