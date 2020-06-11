baa1ab589a0034ff646663b3b7f3bf04868dd7ce
david <davidqhr@gmail.com>
2020-06-12 00:16:36 +0800

    fix default fresh kapp namespace not enabled bug

--- api/handler/applications.go
+++ api/handler/applications.go
@@ -191,18 +191,18 @@ func getKappNamespaceFromContext(c echo.Context) (coreV1.Namespace, error) {
 		return coreV1.Namespace{}, err
 	}
 
-	var kappEnabledVal string
-	if application.IsActive {
-		kappEnabledVal = controllers.KappEnableLabelValue
-	} else {
-		kappEnabledVal = "false"
-	}
+	//var kappEnabledVal string
+	//if application.IsActive {
+	//	kappEnabledVal = controllers.KappEnableLabelValue
+	//} else {
+	//	kappEnabledVal = "false"
+	//}
 
 	crdApplication := coreV1.Namespace{
 		ObjectMeta: metaV1.ObjectMeta{
 			Name: application.Name,
 			Labels: map[string]string{
-				controllers.KappEnableLabelName: kappEnabledVal,
+				controllers.KappEnableLabelName: controllers.KappEnableLabelValue,
 			},
 		},
 	}
