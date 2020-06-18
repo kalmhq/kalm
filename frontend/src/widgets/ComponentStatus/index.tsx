eb4552802383a349312dc273355c8355ed69d3fe
david <davidqhr@gmail.com>
2020-06-18 14:25:25 +0800

    Make ComponentStatus a normal box

--- frontend/src/widgets/ComponentStatus/index.tsx
+++ frontend/src/widgets/ComponentStatus/index.tsx
@@ -1,4 +1,14 @@
-import { Box, createStyles, Drawer, IconButton, Paper, Theme, withStyles, WithStyles } from "@material-ui/core";
+import {
+  Box,
+  createStyles,
+  Drawer,
+  IconButton,
+  Paper,
+  Theme,
+  withStyles,
+  WithStyles,
+  Collapse,
+} from "@material-ui/core";
 import { grey } from "@material-ui/core/colors";
 import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
 import ChevronRightIcon from "@material-ui/icons/ChevronRight";
@@ -14,6 +24,7 @@ import { SectionTitle } from "../SectionTitle";
 import { PieChartComponent } from "../PieChart";
 import { APP_BAR_HEIGHT } from "../../layout/AppBar";
 import { setSettingsAction } from "../../actions/settings";
+import { KPanel } from "widgets/KPanel";
 
 const RIGHT_DRAWER_WIDTH = 340;
 
@@ -22,7 +33,7 @@ const styles = (theme: Theme) =>
     root: {
       padding: `0 ${theme.spacing(2)}px`,
       width: "100%",
-      height: "100%"
+      height: "100%",
     },
     componentTitle: {
       display: "flex",
@@ -31,82 +42,82 @@ const styles = (theme: Theme) =>
       // padding: `0 ${theme.spacing(2)}px`,
       height: 35,
       lineHeight: 35,
-      background: "rgba(0, 0, 0, 0.04)"
+      background: "rgba(0, 0, 0, 0.04)",
     },
     chartWrapperOpen: {
       height: 120,
       width: 120,
-      margin: "0 auto"
+      margin: "0 auto",
     },
     chartWrapperClose: {
       height: 58,
       width: 58,
-      margin: "-0 -10px 16px"
+      margin: "-0 -10px 16px",
     },
     podsTitle: {},
     podItem: {
       width: "100%",
       position: "relative",
-      padding: "10px 0"
+      padding: "10px 0",
     },
     podName: {
       display: "flex",
-      alignItems: "center"
+      alignItems: "center",
     },
     podStatus: {
       position: "absolute",
       color: grey[600],
       top: 12,
-      right: 0
+      right: 0,
     },
     podMessage: {
       maxWidth: "100%",
-      whiteSpace: "break-spaces"
+      whiteSpace: "break-spaces",
     },
     drawer: {
       width: RIGHT_DRAWER_WIDTH,
       flexShrink: 0,
-      whiteSpace: "nowrap"
+      whiteSpace: "nowrap",
     },
     drawerPaper: {
       width: RIGHT_DRAWER_WIDTH,
       paddingTop: APP_BAR_HEIGHT,
-      zIndex: 1201
+      zIndex: 1201,
     },
     flexCenter: {
       display: "flex",
       alignItems: "center",
-      justifyContent: "center"
+      justifyContent: "center",
     },
     // <Box boxShadow={3}> doesn't work here, so use style boxShadow
     drawerShadow: {
       boxShadow:
-        "rgba(0, 0, 0, 0.2) 0px 3px 3px -2px, rgba(0, 0, 0, 0.14) 0px 3px 4px 0px, rgba(0, 0, 0, 0.12) 0px 1px 8px 0px"
+        "rgba(0, 0, 0, 0.2) 0px 3px 3px -2px, rgba(0, 0, 0, 0.14) 0px 3px 4px 0px, rgba(0, 0, 0, 0.12) 0px 1px 8px 0px",
     },
     // material-ui official
     drawerOpen: {
       width: RIGHT_DRAWER_WIDTH,
       transition: theme.transitions.create("width", {
         easing: theme.transitions.easing.sharp,
-        duration: theme.transitions.duration.enteringScreen
-      })
+        duration: theme.transitions.duration.enteringScreen,
+      }),
     },
     drawerClose: {
       transition: theme.transitions.create("width", {
         easing: theme.transitions.easing.sharp,
-        duration: theme.transitions.duration.leavingScreen
+        duration: theme.transitions.duration.leavingScreen,
       }),
       overflowX: "hidden",
       width: 70 + 1,
       [theme.breakpoints.up("sm")]: {
-        width: 70 + 1
-      }
-    }
+        width: 70 + 1,
+      },
+    },
   });
 
 const mapStateToProps = (state: RootState) => {
   return {
-    isOpenComponentStatusDrawer: state.get("settings").get("isOpenComponentStatusDrawer")
+    isOpenComponentStatusDrawer: state.get("settings").get("isOpenComponentStatusDrawer"),
   };
 };
 
@@ -193,7 +204,7 @@ class ComponentStatusRaw extends React.PureComponent<Props, State> {
     return {
       podSuccess,
       podPending,
-      podError
+      podError,
     };
   }
 
@@ -233,94 +244,87 @@ class ComponentStatusRaw extends React.PureComponent<Props, State> {
     }
 
     return (
-      <Drawer
-        anchor="right"
-        variant="permanent"
-        className={clsx(classes.drawer, {
-          [classes.drawerOpen]: open,
-          [classes.drawerClose]: !open
-        })}
-        classes={{
-          paper: clsx(classes.drawerPaper, classes.drawerShadow, {
-            [classes.drawerOpen]: open,
-            [classes.drawerClose]: !open
-          })
-        }}>
-        <Paper className={classes.root} square>
-          <div className={`${classes.componentTitle} ${open ? "" : classes.flexCenter}`}>
-            <IconButton
-              onClick={() => dispatch(setSettingsAction({ isOpenComponentStatusDrawer: !open }))}
-              size={"small"}>
-              {open ? <ChevronRightIcon /> : <ChevronLeftIcon />}
-            </IconButton>{" "}
-            {open && this.renderComponentStatus(component)} {open && <H5>{component.get("name")}</H5>}
-          </div>
-          {!open && (
-            <div className={`${classes.componentTitle} ${open ? "" : classes.flexCenter}`}>
-              <H5>Status</H5>
-            </div>
-          )}
-          {!open && (
-            <div className={classes.flexCenter} style={{ marginRight: "-6px", padding: "10px" }}>
-              {this.renderComponentStatus(component)}
-            </div>
-          )}
-          {open ? (
-            <div className={classes.chartWrapperOpen}>
-              <PieChartComponent
-                title={""}
-                labels={["Running", "Pending", "Error"]}
-                data={[pieChartData.podSuccess, pieChartData.podPending, pieChartData.podError]}
-              />
-            </div>
-          ) : (
-            <div className={classes.chartWrapperClose}>
-              <PieChartComponent
-                insideLabel={true}
-                title={""}
-                labels={["Running", "Pending", "Error"]}
-                data={[pieChartData.podSuccess, pieChartData.podPending, pieChartData.podError]}
-              />
-            </div>
-          )}
-          <div className={classes.podsTitle}>
-            <SectionTitle>
-              <H5>Pods</H5>
-            </SectionTitle>
-          </div>
-          {component.get("pods").size > 0 ? (
-            <>
-              {component
-                .get("pods")
-                .filter(pod => pod.get("status") === "Failed")
-                .map(pod => {
-                  return this.renderPodItem(pod);
-                })}
+      <KPanel
+        title={"Component Status"}
+        content={
+          <Box p={2} tutorial-anchor-id="component-from-basic">
+            {/* <Paper className={classes.root} square>*/}
+            {/* <div className={`${classes.componentTitle} ${open ? "" : classes.flexCenter}`}>
+              <IconButton
+                onClick={() => dispatch(setSettingsAction({ isOpenComponentStatusDrawer: !open }))}
+                size={"small"}>
+                {open ? <ChevronRightIcon /> : <ChevronLeftIcon />}
+              </IconButton>{" "}
+              {open && this.renderComponentStatus(component)} {open && <H5>{component.get("name")}</H5>}
+            </div> */}
 
-              {component
-                .get("pods")
-                .filter(pod => pod.get("status") === "Pending")
-                .map(pod => {
-                  return this.renderPodItem(pod);
-                })}
+            {!open && (
+              <div className={`${classes.componentTitle} ${open ? "" : classes.flexCenter}`}>
+                <H5>Status</H5>
+              </div>
+            )}
+            {!open && (
+              <div className={classes.flexCenter} style={{ marginRight: "-6px", padding: "10px" }}>
+                {this.renderComponentStatus(component)}
+              </div>
+            )}
+            {open ? (
+              <div className={classes.chartWrapperOpen}>
+                <PieChartComponent
+                  title={""}
+                  labels={["Running", "Pending", "Error"]}
+                  data={[pieChartData.podSuccess, pieChartData.podPending, pieChartData.podError]}
+                />
+              </div>
+            ) : (
+              <div className={classes.chartWrapperClose}>
+                <PieChartComponent
+                  insideLabel={true}
+                  title={""}
+                  labels={["Running", "Pending", "Error"]}
+                  data={[pieChartData.podSuccess, pieChartData.podPending, pieChartData.podError]}
+                />
+              </div>
+            )}
+            <div className={classes.podsTitle}>
+              <SectionTitle>
+                <H5>Pods</H5>
+              </SectionTitle>
+            </div>
+            {component.get("pods").size > 0 ? (
+              <>
+                {component
+                  .get("pods")
+                  .filter(pod => pod.get("status") === "Failed")
+                  .map(pod => {
+                    return this.renderPodItem(pod);
+                  })}
 
-              {component
-                .get("pods")
-                .filter(pod => pod.get("status") === "Running")
-                .map(pod => {
-                  return this.renderPodItem(pod);
-                })}
+                {component
+                  .get("pods")
+                  .filter(pod => pod.get("status") === "Pending")
+                  .map(pod => {
+                    return this.renderPodItem(pod);
+                  })}
 
-              {component
-                .get("pods")
-                .filter(pod => pod.get("status") === "Succeeded")
-                .map(pod => {
-                  return this.renderPodItem(pod);
-                })}
-            </>
-          ) : null}
-        </Paper>
-      </Drawer>
+                {component
+                  .get("pods")
+                  .filter(pod => pod.get("status") === "Running")
+                  .map(pod => {
+                    return this.renderPodItem(pod);
+                  })}
+
+                {component
+                  .get("pods")
+                  .filter(pod => pod.get("status") === "Succeeded")
+                  .map(pod => {
+                    return this.renderPodItem(pod);
+                  })}
+              </>
+            ) : null}
+          </Box>
+        }
+      />
     );
   }
 }
