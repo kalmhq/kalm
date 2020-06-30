3c7d6ededb52fb4a508d90c4f07f552028bd0e8e
iAladdin <ialaddin@me.com>
2020-07-01 04:38:25 +0800

    Working on Colors

---
+++ frontend/src/colors.ts
@@ -0,0 +1,42 @@
+import { green, indigo, grey, red, amber, yellow } from "@material-ui/core/colors";
+
+export default {
+  primary: indigo[500],
+  primary900: indigo[900],
+  primary800: indigo[800],
+  primary700: indigo[700],
+  primary600: indigo[600],
+  primary400: indigo[400],
+  primary300: indigo[300],
+  primary200: indigo[200],
+  primary100: indigo[100],
+  primary50: indigo[50],
+
+  secondary: amber[500],
+  secondary900: amber[900],
+  secondary800: amber[800],
+  secondary700: amber[700],
+  secondary600: amber[600],
+  secondary400: amber[400],
+  secondary300: amber[300],
+  secondary200: amber[200],
+  secondary100: amber[100],
+  secondary50: amber[50],
+
+  lightest: "#FFFFFF",
+  lighter: "#F8F8F8",
+  light: "#F3F3F3",
+  mediumlight: "#EEEEEE",
+  medium: "#DDDDDD",
+  mediumdark: "#999999",
+  dark: "#666666",
+  darker: "#444444",
+  darkest: "#333333",
+
+  error: red[500],
+  success: green[500],
+  warning: yellow[500],
+
+  textPrimary: grey[900],
+  textSecondary: grey[500],
+};
