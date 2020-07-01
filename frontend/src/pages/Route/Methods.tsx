af9c0afc8bf61619ae1e514137df393a388fd640
wanglei <wlchn@qq.com>
2020-07-01 11:14:58 +0800

    http methods chip

--- frontend/src/pages/Route/Methods.tsx
+++ frontend/src/pages/Route/Methods.tsx
@@ -1,15 +1,28 @@
 import React from "react";
-import { Tooltip } from "@material-ui/core";
+import { Tooltip, Box } from "@material-ui/core";
 import Immutable from "immutable";
+import { KChip } from "widgets/Chip";
+import { WhiteTooltip } from "forms/Application/KappTooltip";
+import { httpMethods } from "types/route";
 
 export const Methods = ({ methods }: { methods: Immutable.List<string> }) => {
-  if (methods.size < 3) {
-    return <span>{methods.join(", ")}</span>;
-  } else {
-    return (
-      <Tooltip title={methods.join(", ")}>
-        <span>{methods.slice(0, 2).join(", ") + ` (${methods.size - 2} more)`}</span>
-      </Tooltip>
-    );
-  }
+  return (
+    <WhiteTooltip
+      title={
+        <Box display={"flex"} width={300} flexWrap={"wrap"} pt={1}>
+          {httpMethods.map((m) => {
+            return (
+              <Box mr={1} mb={1}>
+                <KChip label={m} color="primary" disabled={!methods.includes(m)} />
+              </Box>
+            );
+          })}
+        </Box>
+      }
+    >
+      <span>
+        <KChip label={methods.size === httpMethods.length ? "All" : "Custom"} color="primary" />
+      </span>
+    </WhiteTooltip>
+  );
 };
