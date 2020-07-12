093cd64ff7e72127d35205c1271d76a9b2e637bc
david <davidqhr@gmail.com>
2020-07-12 22:41:45 +0800

    Update strings.go

--- controller/utils/strings.go
+++ controller/utils/strings.go
@@ -1,5 +1,11 @@
 package utils
 
+import (
+	"math/rand"
+	"strings"
+	"time"
+)
+
 // Helper functions to check and remove string from a slice of strings.
 func ContainsString(slice []string, s string) bool {
 	for _, item := range slice {
@@ -18,4 +24,34 @@ func RemoveString(slice []string, s string) (result []string) {
 		result = append(result, item)
 	}
 	return
+}
+
+// The following exists in api project. Consider merge them together later.
+const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+const (
+	letterIdxBits = 6                    // 6 bits to represent a letter index
+	letterIdxMask = 1<<letterIdxBits - 1 // All 1-bits, as many as letterIdxBits
+	letterIdxMax  = 63 / letterIdxBits   // # of letter indices fitting in 63 bits
+)
+
+var src = rand.NewSource(time.Now().UnixNano())
+
+func RandString(n int) string {
+	sb := strings.Builder{}
+	sb.Grow(n)
+	// A src.Int63() generates 63 random bits, enough for letterIdxMax characters!
+	for i, cache, remain := n-1, src.Int63(), letterIdxMax; i >= 0; {
+		if remain == 0 {
+			cache, remain = src.Int63(), letterIdxMax
+		}
+		if idx := int(cache & letterIdxMask); idx < len(letterBytes) {
+			sb.WriteByte(letterBytes[idx])
+			i--
+		}
+		cache >>= letterIdxBits
+		remain--
+	}
+
+	return sb.String()
 }
