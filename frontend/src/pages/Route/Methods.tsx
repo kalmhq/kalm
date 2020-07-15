e05cd7dc0870626654270799320fd66e6059da13
iAladdin <ialaddin@me.com>
2020-07-15 11:52:39 +0800

    remove Unknown Prop Warning for KChip

--- frontend/src/pages/Route/Methods.tsx
+++ frontend/src/pages/Route/Methods.tsx
@@ -10,35 +10,48 @@ export const Methods = ({ methods }: { methods: Immutable.List<string> }) => {
   return (
     <>
       <PopupState variant="popper" popupId="methods-popup-popper">
-        {(popupState) => (
-          <div>
-            <KChip
-              label={methods.size === httpMethods.length ? "All" : "Custom"}
-              color="primary"
-              {...bindHover(popupState)}
-              // for when mouse leave to poper, if not bindPopover will can't dismiss poper
-              {...bindPopover(popupState)}
-            />
+        {(popupState) => {
+          // delete popupState.disableEnforceFocus;
 
-            <Popper {...bindPopper(popupState)} style={{ zIndex: POPPER_ZINDEX }} transition>
-              {({ TransitionProps }) => (
-                <Fade {...TransitionProps} timeout={100}>
-                  <Paper>
-                    <Box display={"flex"} width={300} flexWrap={"wrap"} pt={1} pl={1}>
-                      {httpMethods.map((m) => {
-                        return (
-                          <Box mr={1} mb={1} key={m}>
-                            <KChip label={m} color="primary" disabled={!methods.includes(m)} />
-                          </Box>
-                        );
-                      })}
-                    </Box>
-                  </Paper>
-                </Fade>
-              )}
-            </Popper>
-          </div>
-        )}
+          const hoverProps = bindHover(popupState);
+          const popupProps = bindPopover(popupState);
+          delete popupProps.anchorEl;
+          // @ts-ignore
+          delete popupProps.disableAutoFocus;
+          // @ts-ignore
+          delete popupProps.disableEnforceFocus;
+          // @ts-ignore
+          delete popupProps.disableRestoreFocus;
+          return (
+            <div>
+              <KChip
+                label={methods.size === httpMethods.length ? "All" : "Custom"}
+                color="primary"
+                {...hoverProps}
+                // for when mouse leave to poper, if not bindPopover will can't dismiss poper
+                {...popupProps}
+              />
+
+              <Popper {...bindPopper(popupState)} style={{ zIndex: POPPER_ZINDEX }} transition>
+                {({ TransitionProps }) => (
+                  <Fade {...TransitionProps} timeout={100}>
+                    <Paper>
+                      <Box display={"flex"} width={300} flexWrap={"wrap"} pt={1} pl={1}>
+                        {httpMethods.map((m) => {
+                          return (
+                            <Box mr={1} mb={1} key={m}>
+                              <KChip label={m} color="primary" disabled={!methods.includes(m)} />
+                            </Box>
+                          );
+                        })}
+                      </Box>
+                    </Paper>
+                  </Fade>
+                )}
+              </Popper>
+            </div>
+          );
+        }}
       </PopupState>
     </>
   );
