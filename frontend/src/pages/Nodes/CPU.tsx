cba42d1876d162534c404b8200ad6e3dad0e216c
wanglei <wlchn@qq.com>
2020-07-21 17:05:38 +0800

    add nodes top cpu/memory allocated charts

--- frontend/src/pages/Nodes/CPU.tsx
+++ frontend/src/pages/Nodes/CPU.tsx
@@ -3,8 +3,14 @@ import React from "react";
 import { Node } from "types/node";
 import { sizeStringToNumber } from "utils/sizeConv";
 import { ColoredLinearProgress } from "./LinearProgress";
+import Immutable from "immutable";
 
-export const NodeCPU = ({ node }: { node: Node }) => {
+interface Props {
+  node: Node;
+  showDetails?: boolean;
+}
+
+export const NodeCPU = ({ node, showDetails }: Props) => {
   const allocatable = sizeStringToNumber(node.get("status").get("allocatable").get("cpu"));
   const requests = sizeStringToNumber(node.get("allocatedResources").get("requests").get("cpu"));
 
@@ -19,7 +25,30 @@ export const NodeCPU = ({ node }: { node: Node }) => {
         />{" "}
         ({progress.toFixed(2)}%)
       </Box>
-      Allocated {requests.toFixed(2)} Cores, Total allocatable {allocatable.toFixed(2)} Cores.
+      {showDetails
+        ? `Allocated ${requests.toFixed(2)} Cores, Total allocatable ${allocatable.toFixed(2)} Cores.`
+        : null}
+    </Box>
+  );
+};
+
+export const NodesCPU = ({ nodes }: { nodes: Immutable.List<Node> }) => {
+  let allocatable: number = 0;
+  let requests: number = 0;
+
+  nodes.forEach((node) => {
+    allocatable = allocatable + sizeStringToNumber(node.get("status").get("allocatable").get("cpu"));
+    requests = requests + sizeStringToNumber(node.get("allocatedResources").get("requests").get("cpu"));
+  });
+
+  const progress = (requests / allocatable) * 100;
+  return (
+    <Box p={2}>
+      Allocated {requests.toFixed(2)} Cores, Total allocatable {allocatable.toFixed(2)} Cores of {nodes.size} Nodes.
+      <Box pt={1} display="flex" alignItems="center">
+        <ColoredLinearProgress style={{ width: "100%" }} variant="determinate" value={progress} />
+        <Box ml={1}>({(isNaN(progress) ? 0 : progress).toFixed(2)}%)</Box>
+      </Box>
     </Box>
   );
 };
--- frontend/src/pages/Nodes/Memory.tsx
+++ frontend/src/pages/Nodes/Memory.tsx
@@ -3,8 +3,14 @@ import React from "react";
 import { Node } from "types/node";
 import { humanFileSize, sizeStringToNumber } from "utils/sizeConv";
 import { ColoredLinearProgress } from "./LinearProgress";
+import Immutable from "immutable";
 
-export const NodeMemory = ({ node }: { node: Node }) => {
+interface Props {
+  node: Node;
+  showDetails?: boolean;
+}
+
+export const NodeMemory = ({ node, showDetails }: Props) => {
   const allocatable = sizeStringToNumber(node.get("status").get("allocatable").get("memory"));
   const requests = sizeStringToNumber(node.get("allocatedResources").get("requests").get("memory"));
 
@@ -19,7 +25,28 @@ export const NodeMemory = ({ node }: { node: Node }) => {
         />{" "}
         ({progress.toFixed(2)}%)
       </Box>
-      Allocated {humanFileSize(requests)}, Total allocatable {humanFileSize(allocatable)}.
+      {showDetails ? `Allocated ${humanFileSize(requests)}, Total allocatable ${humanFileSize(allocatable)}.` : null}
+    </Box>
+  );
+};
+
+export const NodesMemory = ({ nodes }: { nodes: Immutable.List<Node> }) => {
+  let allocatable: number = 0;
+  let requests: number = 0;
+
+  nodes.forEach((node) => {
+    allocatable = allocatable + sizeStringToNumber(node.get("status").get("allocatable").get("memory"));
+    requests = requests + sizeStringToNumber(node.get("allocatedResources").get("requests").get("memory"));
+  });
+
+  const progress = (requests / allocatable) * 100;
+  return (
+    <Box p={2}>
+      Allocated {humanFileSize(requests)}, Total allocatable {humanFileSize(allocatable)} Cores of {nodes.size} Nodes.
+      <Box pt={1} display="flex" alignItems="center">
+        <ColoredLinearProgress style={{ width: "100%" }} variant="determinate" value={progress} />
+        <Box ml={1}>({(isNaN(progress) ? 0 : progress).toFixed(2)}%)</Box>
+      </Box>
     </Box>
   );
 };
