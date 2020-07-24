9d26acf9faabe50e5f626dc0af1d3e8f49b247d2
david <davidqhr@gmail.com>
2020-07-24 18:37:13 +0800

    update controller Dockerfile

--- api/resources/docker_registry.go
+++ api/resources/docker_registry.go
@@ -133,7 +133,10 @@ func (builder *Builder) CreateDockerRegistry(registry *DockerRegistry) (*DockerR
 		ObjectMeta: metaV1.ObjectMeta{
 			Name: registry.Name,
 		},
-		Spec: *registry.DockerRegistrySpec,
+	}
+
+	if registry.DockerRegistrySpec != nil {
+		dockerRegistry.Spec = *registry.DockerRegistrySpec
 	}
 
 	secret := &coreV1.Secret{
