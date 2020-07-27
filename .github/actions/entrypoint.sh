d36c6244e48436a2adf299afd6d3715d04b628af
Liu Mingmin <liumm@ddex.io>
2020-07-27 21:17:56 +0800

    enable github action

---
+++ .github/actions/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/sh -l
+
+resp_code=$(curl -s -o resp.log -w "%{http_code}" -XPOST ${}/__kalm_webhook/deploy?deploy-key=${KALM_DEPLOY_KEY}\&app=${KALM_APP }\&component=${KALM_COMPONENT}\&image-tag=${KALM_COMPONENT_IMG_TAG})
+
+if [ $resp_code == 200 ] 
+then
+    exit 0
+else
+    cat resp.log
+    exit 1
+fi
+
+#echo "::set-output name=time::$time"
