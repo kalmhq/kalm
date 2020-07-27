65895a3de55443eaec55d0a971eeb9059a87602b
Liu Mingmin <liumm@ddex.io>
2020-07-27 21:37:26 +0800

    set KALM_API_ADDRESS

--- .github/actions/entrypoint.sh
+++ .github/actions/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -l
 
-resp_code=$(curl -s -o resp.log -w "%{http_code}" -XPOST ${}/__kalm_webhook/deploy?deploy-key=${KALM_DEPLOY_KEY}\&app=${KALM_APP }\&component=${KALM_COMPONENT}\&image-tag=${KALM_COMPONENT_IMG_TAG})
+resp_code=$(curl -s -o resp.log -w "%{http_code}" -XPOST ${KALM_API_ADDRESS}/__kalm_webhook/deploy?deploy-key=${KALM_DEPLOY_KEY}\&app=${KALM_APP}\&component=${KALM_COMPONENT}\&image-tag=${KALM_COMPONENT_IMG_TAG})
 
 if [ $resp_code == 200 ] 
 then
