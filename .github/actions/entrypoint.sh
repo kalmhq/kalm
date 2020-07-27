8dc65d71269cb440e2a941e407dd0e7e1049ecb6
Liu Mingmin <liumm@ddex.io>
2020-07-27 21:39:19 +0800

    prefix with: INPUT_

--- .github/actions/entrypoint.sh
+++ .github/actions/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -l
 
-resp_code=$(curl -s -o resp.log -w "%{http_code}" -XPOST ${KALM_API_ADDRESS}/__kalm_webhook/deploy?deploy-key=${KALM_DEPLOY_KEY}\&app=${KALM_APP}\&component=${KALM_COMPONENT}\&image-tag=${KALM_COMPONENT_IMG_TAG})
+resp_code=$(curl -s -o resp.log -w "%{http_code}" -XPOST ${INPUT_KALM_API_ADDRESS}/__kalm_webhook/deploy?deploy-key=${INPUT_KALM_DEPLOY_KEY}\&app=${INPUT_KALM_APP}\&component=${INPUT_KALM_COMPONENT}\&image-tag=${INPUT_KALM_COMPONENT_IMG_TAG})
 
 if [ $resp_code == 200 ] 
 then
