40e64c3b01b3ea52ab0dba76d4ea7ed1db06f0fd
iAladdin <ialaddin@me.com>
2020-07-28 18:52:35 +0800

    Polish border color

--- frontend/src/layout/SecondHeader.tsx
+++ frontend/src/layout/SecondHeader.tsx
@@ -1,5 +1,4 @@
 import { createStyles, Theme } from "@material-ui/core";
-import { grey } from "@material-ui/core/colors";
 import { WithStyles, withStyles } from "@material-ui/styles";
 import React from "react";
 import { connect } from "react-redux";
@@ -20,20 +19,20 @@ const styles = (theme: Theme) =>
       zIndex: SECOND_HEADER_ZINDEX,
       height: SECOND_HEADER_HEIGHT,
       width: "100%",
-      // background: "white",
-      borderBottom: `1px solid ${grey[200]}`,
+      background: theme.palette.background.paper,
+      borderBottom: `1px solid ${theme.palette.divider}`,
       display: "flex",
     },
     left: {
       width: LEFT_SECTION_OPEN_WIDTH,
       height: SECOND_HEADER_HEIGHT,
-      borderRight: `1px solid ${grey[200]}`,
+      borderRight: `1px solid ${theme.palette.divider}`,
     },
     leftTextContainer: {
       display: "flex",
       alignItems: "center",
       paddingLeft: 32,
-      borderRight: `1px solid ${grey[200]}`,
+      borderRight: `1px solid ${theme.palette.divider}`,
     },
     right: {
       flex: 1,
