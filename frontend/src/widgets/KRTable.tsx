ce25dd127e9d3082f795683042ff85ed1d70c2b6
wanglei <wlchn@qq.com>
2020-08-10 20:26:43 +0800

    style react-table

--- frontend/src/widgets/KRTable.tsx
+++ frontend/src/widgets/KRTable.tsx
@@ -1,12 +1,14 @@
-import React from "react";
+import Paper from "@material-ui/core/Paper";
 import MuiTable from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
 import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
 import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
+import React from "react";
 import { useTable } from "react-table";
 
-export const KRTable = ({ columns, data }: { columns: any[]; data: any[] }) => {
+export const KRTable = ({ columns, data }: { columns: { Header: string; accessor: string }[]; data: any[] }) => {
   const columnsMemo = React.useMemo(
     () =>
       [
@@ -51,28 +53,34 @@ export const KRTable = ({ columns, data }: { columns: any[]; data: any[] }) => {
   });
 
   return (
-    <MuiTable {...getTableProps()}>
-      <TableHead>
-        {headerGroups.map((headerGroup) => (
-          <TableRow {...headerGroup.getHeaderGroupProps()}>
-            {headerGroup.headers.map((column) => (
-              <TableCell {...column.getHeaderProps()}>{column.render("Header")}</TableCell>
-            ))}
-          </TableRow>
-        ))}
-      </TableHead>
-      <TableBody>
-        {rows.map((row, i) => {
-          prepareRow(row);
-          return (
-            <TableRow {...row.getRowProps()}>
-              {row.cells.map((cell) => {
-                return <TableCell {...cell.getCellProps()}>{cell.render("Cell")}</TableCell>;
-              })}
+    <TableContainer component={Paper} variant="outlined" square>
+      <MuiTable {...getTableProps()}>
+        <TableHead>
+          {headerGroups.map((headerGroup) => (
+            <TableRow {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column) => (
+                <TableCell {...column.getHeaderProps()}>{column.render("Header")}</TableCell>
+              ))}
             </TableRow>
-          );
-        })}
-      </TableBody>
-    </MuiTable>
+          ))}
+        </TableHead>
+        <TableBody>
+          {rows.map((row, i) => {
+            prepareRow(row);
+            return (
+              <TableRow {...row.getRowProps()}>
+                {row.cells.map((cell) => {
+                  return (
+                    <TableCell {...cell.getCellProps()} style={i === rows.length - 1 ? { borderBottom: "none" } : {}}>
+                      {cell.render("Cell")}
+                    </TableCell>
+                  );
+                })}
+              </TableRow>
+            );
+          })}
+        </TableBody>
+      </MuiTable>
+    </TableContainer>
   );
 };
