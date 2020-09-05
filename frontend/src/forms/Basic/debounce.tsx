3715c354fce80d67ed895a990079bd1f7724cc9c
wanglei <wlchn@qq.com>
2020-09-05 10:18:54 +0800

    fix field touched

--- frontend/src/forms/Basic/debounce.tsx
+++ frontend/src/forms/Basic/debounce.tsx
@@ -10,11 +10,13 @@ import { FieldProps, getIn } from "formik";
 const mapStateToProps = (state: RootState, ownProps: FilledTextFieldProps & FieldProps) => {
   const {
     field: { name },
-    form: { touched, errors },
+    form: { touched, errors, submitCount },
   } = ownProps;
   const isDisplayDebounceError = getIsDisplayDebounceError(state, name);
   const error = getIn(errors, name);
-  const _touched = getIn(touched, name);
+  // https://github.com/formium/formik/issues/691#issuecomment-446509600
+  const _touched = getIn(touched, name) || submitCount > 0;
+
   return {
     showError: !!error && (_touched || isDisplayDebounceError),
   };
