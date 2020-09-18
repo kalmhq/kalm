0245edcf9d8c883017de3af1257495493422f823
david <davidqhr@gmail.com>
2020-09-18 23:33:14 +0800

    update install kustomize scripts

---
+++ scripts/intall-kustomize.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# Downloads the most recently released kustomize binary
+# to your current working directory.
+#
+# Fails if the file already exists.
+
+where=$PWD
+if [ -f $where/kustomize ]; then
+  echo "A file named kustomize already exists (remove it first)."
+  exit 1
+fi
+
+tmpDir=`mktemp -d`
+if [[ ! "$tmpDir" || ! -d "$tmpDir" ]]; then
+  echo "Could not create temp dir."
+  exit 1
+fi
+
+function cleanup {
+  rm -rf "$tmpDir"
+}
+
+trap cleanup EXIT
+
+pushd $tmpDir >& /dev/null
+
+opsys=windows
+if [[ "$OSTYPE" == linux* ]]; then
+  opsys=linux
+elif [[ "$OSTYPE" == darwin* ]]; then
+  opsys=darwin
+fi
+
+curl https://github.com/kubernetes-sigs/kustomize/releases/download/kustomize/v3.6.1/kustomize_v3.6.1_${opsys}_amd64.tar.gz -s -O -L
+
+tar xzf ./kustomize_v*_${opsys}_amd64.tar.gz
+
+cp ./kustomize $where
+
+popd >& /dev/null
+
+./kustomize version
+
+echo kustomize installed to current directory.
