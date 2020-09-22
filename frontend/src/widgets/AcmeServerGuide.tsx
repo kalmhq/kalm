f1175cb9597d421ad1e740d7afb8d5d1727559b5
iAladdin <ialaddin@me.com>
2020-09-22 08:38:01 +0800

    new DNSConfigGuide

--- frontend/src/widgets/AcmeServerGuide.tsx
+++ frontend/src/widgets/AcmeServerGuide.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, Button, createStyles, Theme, withStyles } from "@material-ui/core";
+import { Box, Button, createStyles, Grid, Theme, withStyles } from "@material-ui/core";
 import { WithStyles } from "@material-ui/styles";
 import { Alert } from "@material-ui/lab";
 import { Expansion } from "forms/Route/expansion";
@@ -9,92 +9,171 @@ import { Link } from "react-router-dom";
 import { Certificate, dns01Issuer } from "types/certificate";
 import { InfoBox } from "./InfoBox";
 import { KLink } from "./Link";
+import DomainStatus from "./DomainStatus";
+import { FlexRowItemCenterBox } from "./Box";
+import { IconButtonWithTooltip } from "./IconButtonWithTooltip";
+import { CopyIcon } from "./Icon";
+import copy from "copy-to-clipboard";
+import { connect } from "react-redux";
+import { TDispatchProp } from "types";
+import { setSuccessNotificationAction } from "actions/notification";
 
 const styles = (_theme: Theme) =>
   createStyles({
     root: {},
-    actionArea: {
-      padding: _theme.spacing(1),
-      borderRadius: 4,
-      color: "#FFF",
-      backgroundColor: "#000",
-      overflowX: "auto",
-    },
   });
 
-interface AcemServerGuideProps extends WithStyles<typeof styles> {
+interface AcemServerGuideProps extends TDispatchProp, WithStyles<typeof styles> {
   acmeServer: AcmeServerInfo | null;
   cert?: Certificate | undefined;
   showPanel?: boolean;
 }
 
-export const AcmeServerGuide = withStyles(styles)((props: AcemServerGuideProps) => {
-  const { acmeServer, classes, cert, showPanel } = props;
-  if (cert && cert.httpsCertIssuer !== dns01Issuer) {
-    return null;
-  }
-  if (
-    !acmeServer ||
-    (acmeServer.acmeDomain.length === 0 &&
-      acmeServer.ipForNameServer.length === 0 &&
-      acmeServer.nsDomain.length === 0 &&
-      !acmeServer.ready)
-  ) {
-    return (
+export const AcmeServerGuide = connect()(
+  withStyles(styles)((props: AcemServerGuideProps) => {
+    const { acmeServer, cert, showPanel } = props;
+    if (cert && cert.httpsCertIssuer !== dns01Issuer) {
+      return null;
+    }
+    if (
+      !acmeServer ||
+      (acmeServer.acmeDomain.length === 0 &&
+        acmeServer.ipForNameServer.length === 0 &&
+        acmeServer.nsDomain.length === 0 &&
+        !acmeServer.ready)
+    ) {
+      return (
+        <Box p={2}>
+          <Expansion title="ACME DNS Server" defaultUnfold>
+            <Loading />
+            Waiting for staring.
+          </Expansion>
+        </Box>
+      );
+    }
+
+    const options = [
+      {
+        title: <KLink to="/acme">Check and config Kalm DNS Server</KLink>,
+        content: "",
+      },
+    ];
+
+    return !acmeServer.ready || showPanel ? (
       <Box p={2}>
-        <Expansion title="ACME DNS Server" defaultUnfold>
-          <Loading />
-          Waiting for staring.
+        <Expansion title="Kalm DNS Server needs config" defaultUnfold>
+          <Box p={2}>
+            {acmeServer.ready ? (
+              <Alert severity="info">
+                In order to complete the verification and automatic renew of the wildcard certificate, an ACME dns
+                server and udp loadbalancer will be installed on your cluster. Please follow the prompts and configure
+                the following records in your DNS nameserver. Please note that this information is automatically
+                generated based on your input. Read the documentation for more information.{" "}
+                <KLink to={""}>[Learn More]</KLink>
+              </Alert>
+            ) : (
+              <Alert severity="warning">
+                You simply need to do the following to get your DNS server up and running.
+              </Alert>
+            )}
+            <>
+              <Box p={1}>DNS Server Domain:</Box>
+              <Box p={1}>
+                <DNSConfigGuide type={"NS"} domain={acmeServer.acmeDomain} nsRecord={acmeServer.nsDomain} />
+              </Box>
+              <Box p={1}>
+                Shadow Domain:
+                <Box p={1}>
+                  <DNSConfigGuide domain={acmeServer.nsDomain} type="A" cnameRecord={acmeServer.ipForNameServer} />
+                </Box>
+              </Box>
+              <Button color="primary" variant="outlined" size="small" component={Link} to={`/acme/edit`}>
+                Edit
+              </Button>
+            </>
+          </Box>
         </Expansion>
       </Box>
+    ) : (
+      <Box p={2}>
+        <InfoBox title="Kalm DNS Server is running" options={options}></InfoBox>
+      </Box>
     );
-  }
+  }),
+);
 
-  const options = [
-    {
-      title: <KLink to="/acme">Check and config Kalm DNS Server</KLink>,
-      content: "",
+const dnsConfigstyles = (_theme: Theme) =>
+  createStyles({
+    root: {},
+    cell: {
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      flexDirection: "column",
     },
-  ];
+  });
 
-  return !acmeServer.ready || showPanel ? (
-    <Box p={2}>
-      <Expansion title="Kalm DNS Server needs config" defaultUnfold>
-        <Box p={2}>
-          {acmeServer.ready ? (
-            <Alert severity="success">Kalm DNS server is running well, you don't need to do any more.</Alert>
-          ) : (
-            <Alert severity="warning">You simply need to do the following to get your DNS server up and running.</Alert>
-          )}
-          <>
-            <Box p={1}>
-              DNS Server Domain:
-              <Box p={1}>
-                NS Record:
-                <pre className={classes.actionArea}>
-                  {acmeServer.acmeDomain} NS {acmeServer.nsDomain}
-                </pre>
-              </Box>
+interface DNSConfigGuideProps extends TDispatchProp, WithStyles<typeof dnsConfigstyles> {
+  type: string;
+  domain: string;
+  nsRecord?: string;
+  cnameRecord?: string;
+  aRecord?: string;
+}
+
+export const DNSConfigGuide = connect()(
+  withStyles(dnsConfigstyles)((props: DNSConfigGuideProps) => {
+    const { domain, type, nsRecord, cnameRecord, aRecord, dispatch, classes } = props;
+    const record = nsRecord || cnameRecord || aRecord || "";
+    return (
+      <Box p={1} justifyContent="center" display="flex" flexDirection="row" alignItems="center">
+        <Grid container spacing={1}>
+          <Grid item xs={1} sm={1} md={1} className={classes.cell}>
+            <Box>Status</Box>
+            <Box height={44} className={classes.cell}>
+              <DomainStatus domain={domain} nsDomain={nsRecord} cnameDomain={cnameRecord} ipAddress={aRecord} />
             </Box>
-            <Box p={1}>
-              Shadow Domain:
-              <Box p={1}>
-                A Record:
-                <pre className={classes.actionArea}>
-                  {acmeServer.nsDomain} A {acmeServer.ipForNameServer}
-                </pre>
-              </Box>
+          </Grid>
+          <Grid item xs={1} sm={1} md={1} className={classes.cell}>
+            <Box>Type</Box>
+            <Box height={44} width={44} className={classes.cell}>
+              {type}
             </Box>
-            <Button color="primary" variant="outlined" size="small" component={Link} to={`/acme/edit`}>
-              Edit
-            </Button>
-          </>
-        </Box>
-      </Expansion>
-    </Box>
-  ) : (
-    <Box p={2}>
-      <InfoBox title="Kalm DNS Server is running" options={options}></InfoBox>
-    </Box>
-  );
-});
+          </Grid>
+          <Grid item xs={5} sm={5} md={5} className={classes.cell}>
+            <Box>Domain</Box>
+            <FlexRowItemCenterBox>
+              <Box textAlign="center">{domain}</Box>
+              <IconButtonWithTooltip
+                tooltipTitle="Copy"
+                aria-label="copy"
+                onClick={() => {
+                  copy(domain);
+                  dispatch(setSuccessNotificationAction("Copied successful!"));
+                }}
+              >
+                <CopyIcon fontSize="small" />
+              </IconButtonWithTooltip>
+            </FlexRowItemCenterBox>
+          </Grid>
+          <Grid item xs={5} sm={5} md={5} className={classes.cell}>
+            <Box>Record</Box>
+            <FlexRowItemCenterBox>
+              <Box textAlign="center">{record}</Box>
+              <IconButtonWithTooltip
+                tooltipTitle="Copy"
+                aria-label="copy"
+                onClick={() => {
+                  copy(record);
+                  dispatch(setSuccessNotificationAction("Copied successful!"));
+                }}
+              >
+                <CopyIcon fontSize="small" />
+              </IconButtonWithTooltip>
+            </FlexRowItemCenterBox>
+          </Grid>
+        </Grid>
+      </Box>
+    );
+  }),
+);
