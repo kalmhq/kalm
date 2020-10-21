957b83021d9c074d948907b252a1ef7d78944cc7
Liu Mingmin <liumm@ddex.io>
2020-10-21 23:48:49 +0800

    deal with ns create & delete

--- controller/api/buildin/ns_webhook.go
+++ controller/api/buildin/ns_webhook.go
@@ -2,14 +2,17 @@ package buildin
 
 import (
 	"context"
-	"fmt"
+	"k8s.io/api/admission/v1beta1"
 	"net/http"
+	logf "sigs.k8s.io/controller-runtime/pkg/log"
 
 	"github.com/kalmhq/kalm/controller/api/v1alpha1"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
 	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
 )
+
+var nsValidatorLog = logf.Log.WithName("ns-validator")
 
 // +kubebuilder:webhook:verbs=create;update;delete,path=/validate-v1-ns,mutating=false,failurePolicy=fail,groups="",resources=namespaces,versions=v1,name=vns.kb.io
 
@@ -23,23 +26,44 @@ var _ admission.Handler = &NSValidator{}
 var _ admission.DecoderInjector = &NSValidator{}
 
 // NSValidator admits a namespace if a tenant label exists.
+// ref: https://github.com/kubernetes-sigs/controller-runtime/blob/master/pkg/webhook/admission/validator.go#L59
 func (v *NSValidator) Handle(ctx context.Context, req admission.Request) admission.Response {
 
-	fmt.Println("ns webhook called")
+	nsValidatorLog.Info("ns webhook called")
 
 	ns := corev1.Namespace{}
 
-	err := v.decoder.Decode(req, &ns)
-	if err != nil {
-		return admission.Errored(http.StatusBadRequest, err)
+	// Get the object in the request
+	if req.Operation == v1beta1.Create {
+		err := v.decoder.Decode(req, &ns)
+		if err != nil {
+			return admission.Errored(http.StatusBadRequest, err)
+		}
+
+		if isKalmSystemNamespace(ns) {
+			return admission.Allowed("")
+		}
+
+		if err := v1alpha1.AllocateTenantResource(&ns, v1alpha1.ResourceApplicationsCount, resource.MustParse("1")); err != nil {
+			return admission.Errored(http.StatusBadRequest, err)
+		}
 	}
 
-	if isKalmSystemNamespace(ns) {
+	if req.Operation == v1beta1.Update {
 		return admission.Allowed("")
 	}
 
-	if err := v1alpha1.AllocateTenantResource(&ns, v1alpha1.ResourceApplicationsCount, resource.MustParse("1")); err != nil {
-		return admission.Errored(http.StatusBadRequest, err)
+	if req.Operation == v1beta1.Delete {
+		// In reference to PR: https://github.com/kubernetes/kubernetes/pull/76346
+		// OldObject contains the object being deleted
+		err := v.decoder.DecodeRaw(req.OldObject, &ns)
+		if err != nil {
+			return admission.Errored(http.StatusBadRequest, err)
+		}
+
+		if err := v1alpha1.ReleaseTenantResource(&ns, v1alpha1.ResourceApplicationsCount, resource.MustParse("1")); err != nil {
+			return admission.Errored(http.StatusBadRequest, err)
+		}
 	}
 
 	return admission.Allowed("")
