0f066ad6bb5675b0c96db35b55861e42b66acb7c
david <davidqhr@gmail.com>
2020-11-17 17:17:23 +0800

    fix typo

--- frontend/src/layout/DashboardLayout.tsx
+++ frontend/src/layout/DashboardLayout.tsx
@@ -3,7 +3,7 @@ import { createStyles, Theme, withStyles, WithStyles } from "@material-ui/core/s
 import clsx from "clsx";
 import { WithData } from "hoc/withData";
 import { TutorialDrawer } from "pages/Tutorial";
-import { RequireAuthorizated } from "permission/Authorization";
+import { RequireAuthorizated as RequireAuthorized } from "permission/Authorization";
 import React from "react";
 import { connect } from "react-redux";
 import { RootState } from "reducers";
@@ -95,4 +95,4 @@ class DashboardLayoutRaw extends React.PureComponent<Props> {
   }
 }
 
-export const DashboardLayout = withStyles(styles)(RequireAuthorizated(connect(mapStateToProps)(DashboardLayoutRaw)));
+export const DashboardLayout = withStyles(styles)(RequireAuthorized(connect(mapStateToProps)(DashboardLayoutRaw)));
