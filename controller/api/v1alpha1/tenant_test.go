567e82781034a1f7e39e741d4e45514118897c96
Liu Mingmin <liumm@ddex.io>
2020-11-19 15:57:44 +0800

    TestIsResourceUsageExactlySame

--- controller/api/v1alpha1/tenant_test.go
+++ controller/api/v1alpha1/tenant_test.go
@@ -2,9 +2,12 @@ package v1alpha1
 
 import (
 	"fmt"
-	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"reflect"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"k8s.io/apimachinery/pkg/api/resource"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 func TestA(t *testing.T) {
@@ -25,4 +28,65 @@ func TestA(t *testing.T) {
 
 	name = val.FieldByName("Name")
 	fmt.Printf("--------%+v\n", name)
+}
+
+type QuotaUsageTest struct {
+	UsedQuota1 ResourceList
+	UsedQuota2 ResourceList
+	IsSame     bool
+}
+
+func TestIsResourceUsageExactlySame(t *testing.T) {
+	tests := []QuotaUsageTest{
+		{
+			map[ResourceName]resource.Quantity{
+				ResourceCPU:               resource.MustParse("1"),
+				ResourceAccessTokensCount: resource.MustParse("9"),
+			},
+			map[ResourceName]resource.Quantity{
+				ResourceCPU:               resource.MustParse("1"),
+				ResourceAccessTokensCount: resource.MustParse("9"),
+			},
+			true,
+		},
+		{
+			map[ResourceName]resource.Quantity{
+				ResourceCPU:               resource.MustParse("1"),
+				ResourceAccessTokensCount: resource.MustParse("9"),
+			},
+			map[ResourceName]resource.Quantity{
+				ResourceCPU:                   resource.MustParse("1"),
+				ResourceAccessTokensCount:     resource.MustParse("9"),
+				ResourceDockerRegistriesCount: resource.MustParse("0"),
+			},
+			true,
+		},
+		{
+			map[ResourceName]resource.Quantity{
+				ResourceCPU:               resource.MustParse("1"),
+				ResourceAccessTokensCount: resource.MustParse("9"),
+			},
+			map[ResourceName]resource.Quantity{
+				ResourceCPU:                   resource.MustParse("1"),
+				ResourceDockerRegistriesCount: resource.MustParse("0"),
+			},
+			false,
+		},
+	}
+
+	for i, test := range tests {
+		tenant1 := Tenant{
+			Status: TenantStatus{
+				UsedResourceQuota: test.UsedQuota1,
+			},
+		}
+		tenant2 := Tenant{
+			Status: TenantStatus{
+				UsedResourceQuota: test.UsedQuota2,
+			},
+		}
+
+		actual := isResourceUsageExactlySame(tenant1, tenant2)
+		assert.Equal(t, test.IsSame, actual, fmt.Sprintf("test %d failed", i))
+	}
 }
