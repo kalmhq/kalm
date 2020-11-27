9453943d8eea44386e0aa43aff3c7ac3aadf8162
Liu Mingmin <liumm@ddex.io>
2020-11-27 22:13:21 +0800

    fill logic for ClusterResourceQuotaReconciler

--- controller/controllers/clusterresourcequota_controller.go
+++ controller/controllers/clusterresourcequota_controller.go
@@ -18,7 +18,12 @@ package controllers
 import (
 	"context"
 
+	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/types"
 	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 
 	"github.com/kalmhq/kalm/controller/api/v1alpha1"
 )
@@ -40,7 +45,6 @@ func NewClusterResourceQuotaReconciler(mgr ctrl.Manager) *ClusterResourceQuotaRe
 // +kubebuilder:rbac:groups=core.kalm.dev,resources=clusterresourcequota/status,verbs=get;update;patch
 
 func (r *ClusterResourceQuotaReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
-	_ = context.Background()
 	log := r.Log.WithValues("clusterresourcequota", req.NamespacedName)
 
 	if req.Name != v1alpha1.ClusterResourceQuotaName {
@@ -48,16 +52,52 @@ func (r *ClusterResourceQuotaReconciler) Reconcile(req ctrl.Request) (ctrl.Resul
 		return ctrl.Result{}, nil
 	}
 
-	//todo
+	var clusterResourceQuota v1alpha1.ClusterResourceQuota
+	if err := r.Get(r.ctx, client.ObjectKey{Name: v1alpha1.ClusterResourceQuotaName}, &clusterResourceQuota); err != nil {
+		if errors.IsNotFound(err) {
+			return ctrl.Result{}, nil
+		}
+
+		return ctrl.Result{}, err
+	}
+
 	// list and sum tenant, update status.usedResource
+	var tenantList v1alpha1.TenantList
+	if err := r.List(r.ctx, &tenantList); err != nil {
+		return ctrl.Result{}, err
+	}
+
+	var usedResourceList v1alpha1.ResourceList
+	for _, tenant := range tenantList.Items {
+		usedResourceList = v1alpha1.SumResourceList(usedResourceList, tenant.Status.UsedResourceQuota)
+	}
+
+	copied := clusterResourceQuota.DeepCopy()
+	copied.Status.UsedResourceQuota = usedResourceList
+	if err := r.Status().Update(r.ctx, &clusterResourceQuota); err != nil {
+		return ctrl.Result{}, err
+	}
 
 	return ctrl.Result{}, nil
+}
+
+type toSingletonClusterResourceQuotaMapper struct{}
+
+func (_ toSingletonClusterResourceQuotaMapper) Map(_ handler.MapObject) []reconcile.Request {
+	return []reconcile.Request{
+		{
+			NamespacedName: types.NamespacedName{
+				Name: v1alpha1.ClusterResourceQuotaName,
+			},
+		},
+	}
 }
 
 func (r *ClusterResourceQuotaReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&v1alpha1.ClusterResourceQuota{}).
-		//todo watch Tenants
-		// Watches(&v1alpha1.Tenant{}, ).
+		Watches(genSourceForObject(&v1alpha1.Tenant{}), &handler.EnqueueRequestsFromMapFunc{
+			ToRequests: toSingletonClusterResourceQuotaMapper{},
+		}).
 		Complete(r)
 }
