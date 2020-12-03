5995aeb8510774cb504b2fb04fa62e361d862be5
david <davidqhr@gmail.com>
2020-12-03 22:47:16 +0800

    update routes form

--- frontend/src/forms/Final/util.tsx
+++ frontend/src/forms/Final/util.tsx
@@ -1,14 +1,19 @@
-import { FormSpy } from "react-final-form";
-import React from "react";
 import { Box } from "@material-ui/core";
+import React from "react";
+import { FormSpy } from "react-final-form";
 import { CodeBlock } from "widgets/CodeBlock";
 
 export const FormDataPreview = () => {
   return process.env.REACT_APP_DEBUG === "true" ? (
     <Box mt={2} mb={2}>
-      <FormSpy subscription={{ values: true }}>
-        {({ values }: { values: any }) => {
-          return <CodeBlock>{JSON.stringify(values, undefined, 2)}</CodeBlock>;
+      <FormSpy>
+        {({ values, errors }) => {
+          return (
+            <>
+              <CodeBlock>{JSON.stringify(values, undefined, 2)}</CodeBlock>
+              <CodeBlock>{JSON.stringify(errors, undefined, 2)}</CodeBlock>
+            </>
+          );
         }}
       </FormSpy>
     </Box>
---
+++ frontend/src/forms/Route/Domains.tsx
@@ -0,0 +1,154 @@
+import { Box, Button, Grid } from "@material-ui/core";
+import InputAdornment from "@material-ui/core/InputAdornment/InputAdornment";
+import { FinalSelectField } from "forms/Final/select";
+import { FinalTextField } from "forms/Final/textfield";
+import { trimAndToLowerParse } from "forms/normalizer";
+import React from "react";
+import { Field } from "react-final-form";
+import { FieldArray } from "react-final-form-arrays";
+import { useSelector } from "react-redux";
+import { RootState } from "reducers";
+import { Domain } from "types/domains";
+import { AddIcon, DeleteIcon } from "widgets/Icon";
+import { IconButtonWithTooltip } from "widgets/IconButtonWithTooltip";
+import { ValidatorArrayOfIsDNS1123SubDomain, ValidatorRequired } from "../validator";
+
+export const RouteDomains: React.FC = () => {
+  const domains: Domain[] = useSelector((state: RootState) => state.domains.domains);
+  const domainsMap: { [key: string]: Domain } = {};
+  domains.forEach((x) => (domainsMap[x.domain] = x));
+
+  // a.com -> a.com
+  // *.foo.bar -> .foo.bar
+  // *alice.foo.bar -> --alice.foo.bar
+  const normalizeWildcardDomain = (domain: string) => {
+    if (domain.startsWith("*")) {
+      if (domain.length > 1 && domain[1] !== ".") {
+        return "--" + domain.slice(1);
+      } else {
+        return domain.slice(1);
+      }
+    }
+
+    return domain;
+  };
+
+  const renderField = (domain: string, index: number) => {
+    if (domain === "") {
+      return (
+        <Field
+          name={`hosts[${index}]`}
+          component={FinalSelectField}
+          label="Select a domain"
+          options={domains.map((x) => ({
+            value: normalizeWildcardDomain(x.domain),
+            text: x.domain,
+          }))}
+        />
+      );
+    }
+
+    const exactMatchedDomain = domainsMap[domain];
+    if (exactMatchedDomain && !exactMatchedDomain.domain.startsWith("*")) {
+      return (
+        <Field
+          name={`hosts[${index}]`}
+          disabled
+          component={FinalTextField}
+          label="Domain"
+          validate={ValidatorRequired}
+        />
+      );
+    }
+
+    for (let x of domains) {
+      if (!x.domain.startsWith("*")) {
+        continue;
+      }
+
+      const domainStaticSuffix = normalizeWildcardDomain(x.domain);
+
+      if (domain.endsWith(domainStaticSuffix)) {
+        const userInput = domain.slice(0, domain.length - domainStaticSuffix.length);
+
+        if (userInput.includes(".")) {
+          continue;
+        }
+
+        const format = (value: string) => {
+          return value.slice(0, domain.length - domainStaticSuffix.length);
+        };
+
+        const parse = (value: string) => {
+          if (value.startsWith(".")) {
+            value = value.slice(1);
+          }
+
+          if (value.endsWith(".")) {
+            value = value.slice(0, value.length - 1);
+          }
+
+          value = trimAndToLowerParse(value);
+
+          return value + domainStaticSuffix;
+        };
+
+        return (
+          <Field
+            name={`hosts[${index}]`}
+            parse={parse}
+            format={format}
+            autoFocus
+            component={FinalTextField}
+            endAdornment={<InputAdornment position="end">{domainStaticSuffix}</InputAdornment>}
+            label="Domain"
+          />
+        );
+      }
+    }
+
+    return <Field name={`hosts[${index}]`} component={FinalTextField} label="Domain" validate={ValidatorRequired} />;
+  };
+
+  return (
+    <FieldArray<string, any>
+      validate={ValidatorArrayOfIsDNS1123SubDomain}
+      name="hosts"
+      render={({ fields }) => (
+        <div>
+          <Box display="flex" mb={1}>
+            <Button
+              variant="outlined"
+              color="primary"
+              startIcon={<AddIcon />}
+              size="small"
+              onClick={() => {
+                fields.push("");
+              }}
+            >
+              Add a domain
+            </Button>
+          </Box>
+          {fields.value &&
+            fields.value.map((host, index) => (
+              <Grid container spacing={1} key={index}>
+                <Grid item xs={10}>
+                  {renderField(host, index)}
+                </Grid>
+                <Grid item md={2}>
+                  <IconButtonWithTooltip
+                    tooltipPlacement="top"
+                    tooltipTitle="Delete"
+                    aria-label="delete"
+                    onClick={() => fields.remove(index)}
+                  >
+                    <DeleteIcon />
+                  </IconButtonWithTooltip>
+                </Grid>
+              </Grid>
+            ))}
+        </div>
+      )}
+    />
+  );
+};
