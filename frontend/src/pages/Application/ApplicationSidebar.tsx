456f76df232918c9cd344a335ab554773d408cea
wanglei <wlchn@qq.com>
2020-12-16 18:31:24 +0800

    Update ApplicationSidebar.tsx

--- frontend/src/pages/Application/ApplicationSidebar.tsx
+++ frontend/src/pages/Application/ApplicationSidebar.tsx
@@ -14,6 +14,7 @@ import { DashboardIcon, KalmComponentsIcon, PeopleIcon } from "widgets/Icon";
 const mapStateToProps = (state: RootState) => {
   return {
     activeNamespaceName: state.namespaces.active,
+    mode: state.extraInfo.info.mode,
   };
 };
 
@@ -56,14 +57,14 @@ class ApplicationViewDrawerRaw extends React.PureComponent<Props, State> {
   }
 
   private getMenuData() {
-    const { activeNamespaceName, canManageNamespace: canManageScope } = this.props;
+    const { activeNamespaceName, canManageNamespace: canManageScope, mode } = this.props;
     const menus = [];
     menus.push({
       text: "Components",
       to: "/applications/" + activeNamespaceName + "/components",
       icon: <KalmComponentsIcon />,
     });
-    if (canManageScope(activeNamespaceName)) {
+    if (mode === "local" && canManageScope(activeNamespaceName)) {
       menus.push({
         text: sc.APP_MEMBERS_PAGE_NAME,
         to: "/applications/" + activeNamespaceName + "/members",
