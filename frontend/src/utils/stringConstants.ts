de9e4e7b00f876de659a55c2474eb3097c9cb819
iAladdin <ialaddin@me.com>
2020-12-17 23:38:58 +0800

    Add quick link to routes config

--- frontend/src/utils/stringConstants.ts
+++ frontend/src/utils/stringConstants.ts
@@ -9,7 +9,7 @@ export const StringConstants = {
   LIMIT_NOT_SET: "Limit Not Set",
   REQUEST_NOT_SET: "Request Not Set",
   NAME_RULE: "Names can only use digits(0-9), lowercase letters(a-z), and dashes(-). Maximum length is 63 characters.",
-  PORT_ROUTE_QUESTION: "Want to have your container accessible to external sources?",
+  PORT_ROUTE_QUESTION: "Want to access your application / service with a domain?",
   COMPONENT_TYPE_SERVICE_OPTION: "Default choice - Suitable for most continuous services",
   COMPONENT_TYPE_CRONJOB_OPTION: "Scheduled tasks to be ran at specific times",
   COMPONENT_TYPE_DAEMON_OPTION: "For system services which should be deployed once per node",
