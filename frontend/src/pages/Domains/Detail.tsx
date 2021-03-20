ad3c7b098712eb6e657c4efd9d3568f758a5aec2
iAladdin <ialaddin@me.com>
2021-03-20 23:00:08 +0800

    fixed KAL-197

--- frontend/src/pages/Domains/Detail.tsx
+++ frontend/src/pages/Domains/Detail.tsx
@@ -1,6 +1,5 @@
 import { Typography } from "@material-ui/core";
 import Box from "@material-ui/core/Box/Box";
-import { createCertificateAction } from "actions/certificate";
 import { deleteDomainAction } from "actions/domains";
 import { setSuccessNotificationAction } from "actions/notification";
 import { push } from "connected-react-router";
@@ -11,9 +10,9 @@ import { useDispatch, useSelector } from "react-redux";
 import { useRouteMatch } from "react-router-dom";
 import { RootState } from "reducers";
 import CustomButton from "theme/Button";
-import { Certificate, issuerManaged } from "types/certificate";
+import { Certificate } from "types/certificate";
 import { DNSConfigItems } from "widgets/ACMEServer";
-import { HelpIcon, KalmCertificatesIcon } from "widgets/Icon";
+import { HelpIcon } from "widgets/Icon";
 import { DeleteButtonWithConfirmPopover } from "widgets/IconWithPopover";
 import { KPanel } from "widgets/KPanel";
 import { Loading } from "widgets/Loading";
@@ -49,23 +48,6 @@ const DomainDetailPageRaw: React.FC = () => {
     cert = certificates.find((x) => !!x.domains.find((y) => y === domain.domain));
   }
 
-  const applyCert = async () => {
-    await dispatch(
-      createCertificateAction(
-        {
-          name: "",
-          domains: [domain.domain],
-          managedType: issuerManaged,
-          selfManagedCertContent: "",
-          selfManagedCertPrivateKey: "",
-        },
-        false,
-      ),
-    );
-
-    dispatch(push("/domains/" + domain.name + "/tour"));
-  };
-
   const deleteDomain = async () => {
     await dispatch(deleteDomainAction(domain.name));
     dispatch(setSuccessNotificationAction(`Successfully deleted domain ${domain.domain}`));
@@ -90,19 +72,6 @@ const DomainDetailPageRaw: React.FC = () => {
       secondHeaderRight={
         canEditCluster() ? (
           <>
-            {!cert ? (
-              <CustomButton
-                startIcon={<KalmCertificatesIcon />}
-                color="primary"
-                variant="outlined"
-                size="small"
-                tutorial-anchor-id="add-domain"
-                onClick={applyCert}
-              >
-                Apply SSL Certificate
-              </CustomButton>
-            ) : null}
-
             <CustomButton
               startIcon={<HelpIcon />}
               color="primary"
@@ -110,7 +79,7 @@ const DomainDetailPageRaw: React.FC = () => {
               size="small"
               onClick={() => dispatch(push("/domains/" + domain.name + "/tour"))}
             >
-              Setup Tour
+              Configure
             </CustomButton>
 
             <DeleteButtonWithConfirmPopover
