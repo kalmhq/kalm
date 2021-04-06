fcc6d7c7ba2449ed33ac7a5cae925206cd4a4430
david <davidqhr@gmail.com>
2021-04-06 20:14:25 +0800

    remove stories

--- frontend/src/_stories/widgets/InfoBox.stories.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { InfoBox } from "../../widgets/InfoBox";
-export default {
-  title: "Widgets/InfoBox",
-  component: InfoBox,
-};
-
-export const Basic = () => <InfoBox title="Private Registry" options={[]} guideLink="google.com"></InfoBox>;
--- frontend/src/_stories/widgets/Template.stories.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { storiesOf } from "@storybook/react";
-import { SecretValueLabel } from "widgets/Label";
-
-storiesOf("Widgets/Template", module).add("Short Secure Label", () => {
-  return <SecretValueLabel>123123123</SecretValueLabel>;
-});
